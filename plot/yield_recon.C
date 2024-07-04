41c18983bf01538b0ab64fd08cace3c83234fd2a
Bo Yu <by51@duke.edu>
2024-07-04 16:23:51 -0400

    Reorder a few lines in yield_recon.C

--- plot/yield_recon.C
+++ plot/yield_recon.C
@@ -26,7 +26,7 @@ using namespace RooFit;
 
 double get_yield(double energy_low, double energy_high, double theta_low, double theta_high, TTree *input_tree)
 {
-    
+
     TH1F *hist = new TH1F("hist", "hist", 100, 0.4, 1.4);
     input_tree->Draw("MissingPMinus>>hist", Form("WeightFactor*(MissingPMinus>0.5 && MissingPMinus<1.3 && BeamEnergy>%f && BeamEnergy<%f && thetaCM>%f && thetaCM<%f)", energy_low, energy_high, theta_low, theta_high));
 
@@ -59,9 +59,9 @@ double get_yield(double energy_low, double energy_high, double theta_low, double
 int yield_recon()
 {
     TFile *input_file = new TFile("/work/halld2/home/boyu/src_analysis/selection/output/flattree_piminus_p_2H_sim_samecut.root", "read");
-	TTree *input_tree = (TTree*) input_file->Get("flattree_piminus_p_2H_sim");
+    FILE *yieldfile = fopen("output/yield_piminus_p_2H_sim_samecut.txt","w");
+    TTree *input_tree = (TTree*) input_file->Get("flattree_piminus_p_2H_sim");
     TCanvas *canvas = new TCanvas("c1", "c1", 800, 600);
-    FILE *yieldfile = fopen("output/yield_piminus_p_2H_sim.txt","w");
 
     double energy_low  = 6.0;
     double energy_high = 10.5;
@@ -85,11 +85,11 @@ int yield_recon()
             this_yield = get_yield(this_energy, this_energy + energy_step, this_theta, this_theta + theta_step, input_tree);
             fprintf(yieldfile, "%f %f %f %f %f\n", this_energy, this_energy + energy_step, this_theta, this_theta + theta_step, this_yield);
             canvas->Update();
-            canvas->Print("output/fit_piminus_p_2H_sim.pdf(");
+            canvas->Print("output/fit_piminus_p_2H_sim_samecut.pdf(");
             canvas->Clear();
         }
     }
-    canvas->Print("output/fit_piminus_p_2H_sim.pdf)");
+    canvas->Print("output/fit_piminus_p_2H_sim_samecut.pdf)");
 
     return 0;
 
