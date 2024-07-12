ae275f6cc8147a93e23706c72a47dd6a3f1e676d
Bo Yu <by51@duke.edu>
2024-07-12 00:37:55 -0400

    Test DSelector of coherent production on incoherent production from protons

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_gd_kpkmmissd__B4_F4_T1_S4/merged/tree_gd_kpkmmissd__B4_F4_T1_S4_090213.root"
-TREE_NAME="gd_kpkmmissd__B4_F4_T1_S4_Tree"
+# INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_gd_kpkmmissd__B4_F4_T1_S4/merged/tree_gd_kpkmmissd__B4_F4_T1_S4_090213.root"
+INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_kpkmprotmissn__B4_F4_T2_S5/merged/tree_gd_kpkmprotmissn__B4_F4_T2_S5_090213.root"
+# TREE_NAME="gd_kpkmmissd__B4_F4_T1_S4_Tree"
+TREE_NAME="gd_kpkmprotmissn__B4_F4_T2_S5_Tree"
 SELECTOR_FILE="phi_c_2H_data"
 NUM_THREADS=1
-TAG="3_measured"
+TAG="4_measured_incoherent"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.17.0.xml
