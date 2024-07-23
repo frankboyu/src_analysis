5838a13e3f3f3061fd167258957726236c57b368
Bo Yu <by51@duke.edu>
2024-07-23 10:24:42 -0400

    Correct the script to run the skim on a single run

--- skim/run_skim.sh
+++ skim/run_skim.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 REACTION=$1
+RUN=$2
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-python launch.py configs/jobs_root_analysis_${REACTION}.cfg 90000 90662
-swif2 run -workflow src_analysis_selection -maxconcurrent 1000
+python launch.py configs/jobs_analysis_${REACTION}.cfg ${RUN} ${RUN}
+swif2 run -workflow src_analysis_skim
