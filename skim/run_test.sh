941cdfb175fed6f4c4aa5c7fa38c033d85f56a9a
Bo Yu <by51@duke.edu>
2024-07-23 09:56:48 -0400

    Correct the phi_c_2H config file and finish the script to run the test skim

--- skim/run_test.sh
+++ skim/run_test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_gd_kpkmmissd__B4_F4_T1_S4/merged/tree_gd_kpkmmissd__B4_F4_T1_S4_090213.root"
-# INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_kpkmprotmissn__B4_F4_T2_S5/merged/tree_gd_kpkmprotmissn__B4_F4_T2_S5_090213.root"
-TREE_NAME="gd_kpkmmissd__B4_F4_T1_S4_Tree"
-# TREE_NAME="gd_kpkmprotmissn__B4_F4_T2_S5_Tree"
-SELECTOR_FILE="phi_c_2H_data"
-NUM_THREADS=1
-TAG="11_measured_yphicorrected"
+INPUTFILE="/cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm"
+JANA_CONFIG_FILE="phi_c_2H"
+TAG="1_initial"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.17.0.xml
+gxenv $HALLD_VERSIONS/version_5.19.0.xml
 
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C process_tree.C'("'$INPUTFILE'", "'$TREE_NAME'", "'selectors/DSelector_${SELECTOR_FILE}.C+'", '${NUM_THREADS}')'
-
-mv flattree_${SELECTOR_FILE}.root output/flattree_${SELECTOR_FILE}_${TAG}.root
+mkdir output/${JANA_CONFIG_FILE}_${TAG}
+cd output/${JANA_CONFIG_FILE}_${TAG}
+hd_root --config=../../configs/jana_analysis_${JANA_CONFIG_FILE}.cfg ${INPUTFILE}
