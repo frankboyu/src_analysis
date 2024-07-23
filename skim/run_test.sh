3a89bf9b999879c1708976295479dbe917c8e4bd
Bo Yu <by51@duke.edu>
2024-07-23 10:10:33 -0400

    Add script to run singularity container and test to run skim with older software package

--- skim/run_test.sh
+++ skim/run_test.sh
@@ -2,10 +2,10 @@
 
 INPUTFILE="/cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm"
 JANA_CONFIG_FILE="phi_c_2H"
-TAG="1_initial"
+TAG="2_version511"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.19.0.xml
+gxenv $HALLD_VERSIONS/version_5.11.0.xml
 
 mkdir output/${JANA_CONFIG_FILE}_${TAG}
 cd output/${JANA_CONFIG_FILE}_${TAG}
