e020cfe249fbcdde2c8a2402f4100f993c655ba5
Bo Yu <by51@duke.edu>
2024-07-24 10:50:55 -0400

    Move the script of compiling halld_sim to the builds folder

--- sim/run_compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_1.xml
-
-cd /work/halld2/home/boyu/src_software_builds/halld_sim_srcct-4.48.0^hdr438/src
-scons install -j32
-
