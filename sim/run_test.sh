65cb88cc3c12dd1664f0656fdfde8f740f78c7de
Bo Yu <by51@duke.edu>
2024-07-26 13:46:32 -0400

    Various small changes

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -7,6 +7,6 @@ EVENTS=10000
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
-cd output
-gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=2
-rm -r 90*
+# cd output
+python $MCWRAPPER_CENTRAL/gluex_MC.py configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=2 logdir=/farm_out/boyu/src_analysis/sim/
+# rm -r 90*
--- skim/run_test.sh
+++ skim/run_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 INPUTFILE="/cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm"
-JANA_CONFIG_FILE="phi_c_2H"
+REACTION="phi_c_2H"
 TAG="2_version511"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.11.0.xml
 
-mkdir output/${JANA_CONFIG_FILE}_${TAG}
-cd output/${JANA_CONFIG_FILE}_${TAG}
-hd_root --config=../../configs/jana_analysis_${JANA_CONFIG_FILE}.cfg ${INPUTFILE}
+mkdir output/${REACTION}_${TAG}
+cd output/${REACTION}_${TAG}
+hd_root --config=../../configs/jana_analysis_${REACTION}.cfg ${INPUTFILE}
