7b7c5aa7ac8d64015c992e2de3b2e25728d9f75d
Bo Yu <by51@duke.edu>
2024-07-30 17:13:55 -0400

    Edit configs to run piminus_p_2H version 2

--- sim/run_sim.sh
+++ sim/run_sim.sh
@@ -4,15 +4,15 @@ REACTION=$1
 EVENTS=$2
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2 _1.xml
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2_1.xml
 
-python get_configs.py ${REACTION}             # Generate the config files for each individual run.
-python get_number.py ${REACTION} ${EVENTS}    # Generate a text file of the number of events for each individual run
+python3.6 get_configs.py ${REACTION}             # Generate the config files for each individual run.
+python3.6 get_number.py ${REACTION} ${EVENTS}    # Generate a text file of the number of events for each individual run
 
 while read -r run number;
 do
     echo ${run} ${number}
-    gluex_MC.py configs/wrapper_${REACTION}_${run}.cfg ${run} ${number} per_file=1000000 batch=1  logdir=/farm_out/boyu/src_analysis/sim
+    gluex_MC.py configs/wrapper_${REACTION}_${run}.cfg ${run} ${number} per_file=1000000 batch=1 logdir=/farm_out/boyu/src_analysis/sim
 done < list.txt
 
 swif2 run -workflow src_analysis_sim
