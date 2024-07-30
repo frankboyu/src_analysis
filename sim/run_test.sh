c694ce481d85642a5b04ceeea6987fca1d0d34a5
Bo Yu <by51@duke.edu>
2024-07-30 13:36:03 -0400

    Correct typo

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -3,12 +3,12 @@
 REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
-BATCH=0
+BATCH=2
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
-if [${BATCH} == 0]
+if  [ ${BATCH} == "0" ]
 then
     cd output
     python $MCWRAPPER_CENTRAL/gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=${BATCH}
