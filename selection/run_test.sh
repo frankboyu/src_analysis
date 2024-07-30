ff00dd3b3ce82d6ae59490ded6cd6eafec6a764f
Bo Yu <by51@duke.edu>
2024-07-30 17:13:04 -0400

    Edit to test thrown trees

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_gd_kpkmmissd__B4_F4_T1_S4/merged/tree_gd_kpkmmissd__B4_F4_T1_S4_090213.root"
+INPUTFILE="/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_2H_ver01/root/thrown/tree_thrown_gen_MF_090208_000.root"
 # INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_kpkmprotmissn__B4_F4_T2_S5/merged/tree_gd_kpkmprotmissn__B4_F4_T2_S5_090213.root"
-TREE_NAME="gd_kpkmmissd__B4_F4_T1_S4_Tree"
+TREE_NAME="Thrown_Tree"
 # TREE_NAME="gd_kpkmprotmissn__B4_F4_T2_S5_Tree"
-SELECTOR_FILE="phi_c_2H_data"
-NUM_THREADS=1
-TAG="11_measured_yphicorrected"
+SELECTOR_FILE="piminus_p_2H_thrown"
+# NUM_THREADS=1
+TAG="test"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.17.0.xml
 
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C process_tree.C'("'$INPUTFILE'", "'$TREE_NAME'", "'selectors/DSelector_${SELECTOR_FILE}.C+'", '${NUM_THREADS}')'
+root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C process_tree.C'("'$INPUTFILE'", "'$TREE_NAME'", "'selectors/DSelector_${SELECTOR_FILE}.C+'", '1')'
 
 mv flattree_${SELECTOR_FILE}.root output/flattree_${SELECTOR_FILE}_${TAG}.root
