6972a2152eb8868619e402cc146159b14660e2aa
Bo Yu <by51@duke.edu>
2024-07-30 11:53:21 -0400

    Edit the script to test simulation so that either interactively or on the batch system

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -3,10 +3,16 @@
 REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
+BATCH=0
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
-# cd output
-python $MCWRAPPER_CENTRAL/gluex_MC.py configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=2 logdir=/farm_out/boyu/src_analysis/sim/
-# rm -r 90*
+if [${BATCH} == 0]
+then
+    cd output
+    python $MCWRAPPER_CENTRAL/gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=${BATCH}
+    rm -r 90*
+else
+    python $MCWRAPPER_CENTRAL/gluex_MC.py configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=${BATCH} logdir=/farm_out/boyu/src_analysis/sim/
+fi
