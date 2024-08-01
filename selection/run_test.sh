e2926114e306ac902d590490553cdccb04b09163
Bo Yu <by51@duke.edu>
2024-08-01 13:33:10 -0400

    Add and test DSelector of coherent phi on helium

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-INPUTFILE="/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_2H_ver01/root/thrown/tree_thrown_gen_MF_090208_000.root"
-# INPUTFILE="/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_kpkmprotmissn__B4_F4_T2_S5/merged/tree_gd_kpkmprotmissn__B4_F4_T2_S5_090213.root"
-TREE_NAME="Thrown_Tree"
-# TREE_NAME="gd_kpkmprotmissn__B4_F4_T2_S5_Tree"
-SELECTOR_FILE="piminus_p_2H_thrown"
-# NUM_THREADS=1
-TAG="test"
+INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_ghe_kpkmmisshe__B4_F4_T1_S4/merged/tree_ghe_kpkmmisshe__B4_F4_T1_S4_090061.root
+TREE_NAME=ghe_kpkmmisshe__B4_F4_T1_S4_Tree
+SELECTOR_FILE=phi_c_4He_data
+TAG=1_initial
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.17.0.xml
