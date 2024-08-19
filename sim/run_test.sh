a5242881a18b586ad5507874e3bf341b2fc2ecd0
Bo Yu <by51@duke.edu>
2024-08-19 14:33:07 -0400

    Simplify the simulation test script

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -3,16 +3,11 @@
 REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
-BATCH=2
+TAG=4_genout
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
-if  [ ${BATCH} == "0" ]
-then
-    cd output
-    gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=${BATCH}
-    rm -r 90*
-else
-    gluex_MC.py configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=${BATCH} logdir=/farm_out/boyu/src_analysis/sim/
-fi
+mkdir -p output/${REACTION}_test/${TAG}
+cd output/${REACTION}_test/${TAG}
+gluex_MC.py ../../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=0
