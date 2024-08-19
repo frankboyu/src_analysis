4da9ce4743843a6e3434eed4e3eb26ff620a4eb3
Bo Yu <by51@duke.edu>
2024-08-19 13:45:22 -0400

    Test phi_c_2H with more reactions

--- skim/run_test.sh
+++ skim/run_test.sh
@@ -2,11 +2,11 @@
 
 INPUTFILE="/cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm"
 REACTION="phi_c_2H"
-TAG="2_version511"
+TAG="4_morereactions"
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.11.0.xml
+gxenv $HALLD_VERSIONS/version.xml
 
-mkdir output/${REACTION}_${TAG}
-cd output/${REACTION}_${TAG}
-hd_root --config=../../configs/jana_analysis_${REACTION}.cfg ${INPUTFILE}
+mkdir output/${REACTION}_test/${TAG}
+cd output/${REACTION}_test/${TAG}
+hd_root --config=../../../configs/jana_analysis_${REACTION}.cfg ${INPUTFILE}
