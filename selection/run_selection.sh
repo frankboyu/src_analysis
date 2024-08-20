8527097a52413dea31294f4636da5f924df3be29
Bo Yu <by51@duke.edu>
2024-08-20 14:14:27 -0400

    Move job configs to selector folder

--- selection/run_selection.sh
+++ selection/run_selection.sh
@@ -5,5 +5,5 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-python launch.py configs/jobs_root_analysis_${REACTION}.cfg 90000 90662
+python launch.py selectors/jobs_root_analysis_${REACTION}.cfg 90000 90662
 swif2 run -workflow src_analysis_selection -maxconcurrent 1000
