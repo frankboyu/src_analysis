b586725aa9a7cee9be03dafefb68f6913aaceaec
Bo Yu <by51@duke.edu>
2024-08-20 14:09:35 -0400

    Add the delta E variable to the filter

--- plot/filters/filter_phi_c_2H_data.C
+++ plot/filters/filter_phi_c_2H_data.C
@@ -2,9 +2,10 @@ using namespace std;
 using namespace ROOT;
 using namespace RooFit;
 
-double mass_proton = 0.938272;
-double mass_deuteron = 1.875612;
-double mass_pion = 0.13957018;
+double mass_proton      = 0.938272;
+double mass_deuteron    = 1.875612;
+double mass_pion        = 0.13957018;
+double mass_phi         = 1.019461;
 
 int filter_phi_c_2H_data()
 {
@@ -19,12 +20,16 @@ int filter_phi_c_2H_data()
     TLorentzVector *KMinusP4    = new TLorentzVector();
     TLorentzVector *MissingP4   = new TLorentzVector();
     double WeightFactor;
+    int RunNumber, Entry, Combo;
 
     chain->SetBranchAddress("BeamP4", &BeamP4);
     chain->SetBranchAddress("KPlusP4", &KPlusP4);
     chain->SetBranchAddress("KMinusP4", &KMinusP4);
     chain->SetBranchAddress("MissingP4", &MissingP4);
     chain->SetBranchAddress("WeightFactor", &WeightFactor);
+    chain->SetBranchAddress("RunNumber", &RunNumber);
+    chain->SetBranchAddress("Entry", &Entry);
+    chain->SetBranchAddress("Combo", &Combo);
 
     // Create histogram
     TH1F *hist_MPhi                 = new TH1F("hist_MPhi", "hist_MPhi", 400, 0.9, 1.3);
@@ -35,28 +40,33 @@ int filter_phi_c_2H_data()
     TH2F *hist_MPhi_MinusT_After    = new TH2F("hist_MPhi_MinusT_After", "hist_MPhi_MinusT_After", 400, 0.9, 1.3, 200, 0.0, 2.0);
     TH2F *hist_MPhi_yPhi_Before     = new TH2F("hist_MPhi_yPhi_Before", "hist_MPhi_yPhi_Before", 400, 0.9, 1.3, 200, 0.0, 2.0);
     TH2F *hist_MPhi_yPhi_After      = new TH2F("hist_MPhi_yPhi_After", "hist_MPhi_yPhi_After", 400, 0.9, 1.3, 200, 0.0, 2.0);
-    TH2F *hist_MPhi_MRho            = new TH2F("hist_MPhi_MRho", "hist_MPhi_MRho", 400, 0.9, 1.3, 800, 0.6, 1.4);
+    TH2F *hist_MPhi_MRho            = new TH2F("hist_MPhi_MRho", "hist_MPhi_MRho", 400, 0.9, 1.3, 800, 0.3, 1.1);
     TH2F *hist_ThetaPhi_yPhi        = new TH2F("hist_ThetaPhi_yPhi", "hist_ThetaPhi_yPhi", 200, 0.0, 20.0, 200, 0.0, 2.0);
     TH2F *hist_MissingMass_yPhi     = new TH2F("hist_MissingMass_yPhi", "hist_MissingMass_yPhi", 200, 1.0, 3.0, 200, 0.0, 2.0);
+    TH2F *hist_DeltaE_yPhi          = new TH2F("hist_DeltaE_yPhi", "hist_DeltaE_yPhi", 200, -2.0, 2.0, 200, 0.0, 2.0);
 
     // Loop over tree entries
     for (Long64_t i = 0; i < chain->GetEntries(); i++)
+    // for (Long64_t i = 0; i < 10000; i++)
     {
         chain->GetEntry(i);
 
         // Calculate variables
         TLorentzVector *TargetP4 = new TLorentzVector(0.0, 0.0, 0.0, mass_deuteron);
         TLorentzVector *PhiP4 = new TLorentzVector(*KPlusP4 + *KMinusP4);
+        TLorentzVector *PhiP4COM = new TLorentzVector(*KPlusP4 + *KMinusP4);
+        PhiP4COM->Boost(-(*BeamP4+*TargetP4).BoostVector());
         TLorentzVector *KPlusP4AsPion = new TLorentzVector(KPlusP4->X(), KPlusP4->Y(), KPlusP4->Z(), sqrt(KPlusP4->P()*KPlusP4->P() + mass_pion*mass_pion));
         TLorentzVector *KMinusP4AsPion = new TLorentzVector(KMinusP4->X(), KMinusP4->Y(), KMinusP4->Z(), sqrt(KMinusP4->P()*KMinusP4->P() + mass_pion*mass_pion));
 
-        double phi_mass     = (*KPlusP4 + *KMinusP4).M();
-        double rho_mass     = (*KPlusP4AsPion + *KMinusP4AsPion).M();
-        double phi_theta    = (*KPlusP4 + *KMinusP4).Theta() * 180.0 / TMath::Pi();
-        double sqrt_s       = (*BeamP4 + *MissingP4).Mag();
-        double minus_t      = -(*BeamP4 - *PhiP4).Mag2();
-        double minus_u      = -(*BeamP4 - *MissingP4).Mag2();
-        double y_phi        = minus_t/(2*mass_deuteron*(BeamP4->E()-PhiP4->E()));
+        double sqrt_s               = (*BeamP4 + *MissingP4).Mag();
+        double minus_t              = -(*BeamP4 - *PhiP4).Mag2();
+        double minus_u              = -(*BeamP4 - *MissingP4).Mag2();
+        double phi_mass             = (*KPlusP4 + *KMinusP4).M();
+        double phi_energy_expected  = (pow(sqrt_s, 2) - pow(TargetP4->M(), 2) + pow(mass_phi, 2)) / (2. * sqrt_s);
+        double rho_mass             = (*KPlusP4AsPion + *KMinusP4AsPion).M();
+        double phi_theta            = (*KPlusP4 + *KMinusP4).Theta() * 180.0 / TMath::Pi();
+        double y_phi                = minus_t/(2*mass_deuteron*(BeamP4->E()-PhiP4->E()));
 
         // Fill histograms: before filter
         hist_MPhi_ThetaPhi_Before->Fill(phi_mass, phi_theta, WeightFactor);
@@ -76,6 +86,7 @@ int filter_phi_c_2H_data()
         hist_MPhi_MRho->Fill(phi_mass, rho_mass, WeightFactor);
         hist_ThetaPhi_yPhi->Fill(phi_theta, y_phi, WeightFactor);
         hist_MissingMass_yPhi->Fill(MissingP4->M(), y_phi, WeightFactor);
+        hist_DeltaE_yPhi->Fill(phi_energy_expected - PhiP4COM->E(), y_phi, WeightFactor);
     }
 
     // Save histograms to file
@@ -90,6 +101,7 @@ int filter_phi_c_2H_data()
     hist_MPhi_MRho->Write();
     hist_ThetaPhi_yPhi->Write();
     hist_MissingMass_yPhi->Write();
+    hist_DeltaE_yPhi->Write();
     output_file->Close();
 
     return 0;
