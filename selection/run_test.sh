8843ed584fce865d26b06516ff0686c0f5661e36
Bo Yu <by51@duke.edu>
2024-08-21 10:02:53 -0400

    Remake phi_c_2H DSelector and test it

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver05/tree_ghe_kpkmmisshe__B4_F4_T1_S4/merged/tree_ghe_kpkmmisshe__B4_F4_T1_S4_090061.root
-TREE_NAME=ghe_kpkmmisshe__B4_F4_T1_S4_Tree
-SELECTOR_FILE=phi_c_4He_data
-TAG=1_initial
+INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver09/tree_gd_kpkmmissd__B4_F4_T1_S4/merged/tree_gd_kpkmmissd__B4_F4_T1_S4_090213.root
+TREE_NAME=gd_kpkmmissd__B4_F4_T1_S4_Tree
+SELECTOR_FILE=phi_c_2H_data
+TAG=12_remake
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.17.0.xml
