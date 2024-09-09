5a8589f298d9a920d7a7d3100f0a54c4bd547cd8
Bo Yu <by51@duke.edu>
2024-09-09 09:18:48 -0400

    Run test sim

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -3,10 +3,10 @@
 REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
-TAG=4_genout
+TAG=5_mcsmear
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2_1.xml
 
 mkdir -p output/test/${REACTION}_${TAG}
 cd output/test/${REACTION}_${TAG}
