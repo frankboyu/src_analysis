bc5fd82bf3506a2df64f6d3fbbeae6d10a32d2b4
Bo Yu <by51@duke.edu>
2024-09-11 12:58:19 -0400

    Finish and archive running DSelector of phi_c_4He, after reskimming 4 runs with broken files

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/selection/tree_ghe_kpkmmisshe__B4_F4_T1_S4_090629.root
+INPUTFILE=/work/halld2/home/boyu/src_analysis/skim/output/phi_c_4He_ver01/tree_ghe_kpkmmisshe__B4_F4_T1_S4/090656.root
 TREE_NAME=ghe_kpkmmisshe__B4_F4_T1_S4_Tree
 SELECTOR_FILE=phi_c_4He_data
-TAG=2_corrupted
+TAG=090656
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.19.0.xml
