09c27a22378a935bdd756f7fed58c0d4b3b4947b
Bo Yu <by51@duke.edu>
2024-09-16 22:17:12 -0400

    Edit skim config of phi_p_2H to fix batch system issues

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/skim/output/phi_c_4He_ver01/tree_ghe_kpkmhe__B4_F4_T0_S4/090656/*.root
-TREE_NAME=ghe_kpkmhe__B4_F4_T0_S4_Tree
-SELECTOR_FILE=phi_he_4He_data
-TAG=090656
+INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_kpkmprotmissn__B4_F4_T2_S5/merged/tree_gd_kpkmprotmissn__B4_F4_T2_S5_090207.root
+TREE_NAME=gd_kpkmprotmissn__B4_F4_T2_S5_Tree
+SELECTOR_FILE=phi_p_2H_data
+TAG=3_test
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.19.0.xml
+gxenv $HALLD_VERSIONS/version_5.17.0.xml
 
 cd output/test/
 root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREE_NAME'", "'../../selectors/DSelector_${SELECTOR_FILE}.C+'", '1')'
