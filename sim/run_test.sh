8e36ad11cfbe7897d2058d3338d82c264f3bbef2
Bo Yu <by51@duke.edu>
2024-10-03 09:00:14 -0400

    Test new simulation with mc version

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -3,7 +3,7 @@
 REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
-TAG=5_mcsmear
+TAG=6_mc_version
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2_1.xml
