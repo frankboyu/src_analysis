11a238a262c78a1789a88f6872c92e3537718a98
Bo Yu <by51@duke.edu>
2024-10-07 16:26:16 -0400

    Test the coherent phi channel

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-REACTION=piminus_p_2H
+REACTION=phi_d_2H
 RUN=90213
 EVENTS=10000
-TAG=6_mc_version
+TAG=1_initial
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2_1.xml
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
 mkdir -p output/test/${REACTION}_${TAG}
 cd output/test/${REACTION}_${TAG}
