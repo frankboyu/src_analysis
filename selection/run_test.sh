948367fc5d229dd91ff33c1b2d60be1693da100c
Bo Yu <by51@duke.edu>
2024-10-29 14:11:31 -0400

    Re-test the simulation with new t and u cuts

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/test/phi_d_2H_1_initial/root/trees/tree_gd_kpkmmissd__B4_F4_T1_S4_gen_MF_090213_000.root
+INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/test/phi_d_2H_3_newcut/root/trees/tree_gd_kpkmmissd__B4_F4_T1_S4_gen_MF_090213_000.root
 TREE_NAME=gd_kpkmmissd__B4_F4_T1_S4_Tree
 SELECTOR_FILE=phi_c_2H_kpkmmissd
 TAG=sim_test
--- sim/run_test.sh
+++ sim/run_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REACTION=piminus_p_2H
+REACTION=phi_d_2H
 RUN=90213
 EVENTS=10000
-TAG=7_newtest
+TAG=3_newcut
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_2_1.xml
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
 mkdir -p output/test/${REACTION}_${TAG}
 cd output/test/${REACTION}_${TAG}
-gluex_MC.py ../../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=2
+gluex_MC.py ../../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=0
