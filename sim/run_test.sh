8feca9f2249bb25bf63c08a3bbb6d68d4bf2b074
Bo Yu <by51@duke.edu>
2024-11-11 14:47:16 -0500

    Test newest software stack interactively

--- sim/run_test.sh
+++ sim/run_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REACTION=phi_d_2H
+REACTION=piminus_p_2H
 RUN=90213
 EVENTS=10000
-TAG=3_newcut
+TAG=8_newsoftware
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-dev.xml
 
 mkdir -p output/test/${REACTION}_${TAG}
 cd output/test/${REACTION}_${TAG}
-gluex_MC.py ../../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=1
+gluex_MC.py ../../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} per_file=1000000 batch=0
