4ce8239c9703da6e47624076fdf119b65dc6a08f
Bo Yu <by51@duke.edu>
2024-11-12 09:39:35 -0500

    Edit compile script to distinguish the control part

--- sim/run_compile.sh
+++ sim/run_compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-ver01_4.xml
+XML_VERSION=ver01_4
+SIM_VERSION=halld_sim_srcct-4.52.0^rec211111
 
-cd /work/halld2/home/boyu/src_software_builds/halld_sim_srcct-4.52.0^rec211111/src
+source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
+
+cd /work/halld2/home/boyu/src_software_builds/${SIM_VERSION}/src
 scons install -j32
