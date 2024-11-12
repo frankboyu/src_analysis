7cc3547a13fceaf7270f546e41a37173d5df7056
Bo Yu <by51@duke.edu>
2024-11-12 10:32:35 -0500

    The script to test the simulation interactively

---
+++ sim/run_test_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REACTION=piminus_p_2H
+XML_VERSION=devel
+RUN=90213
+EVENTS=10000
+
+source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
+gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
+
+mkdir -p output/test
+cd output/test
+
+gluex_MC.py ../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} batch=0 per_file=1000000
+
+rm -r 90*
