a4b5528289753b3da69196ce7bb908e9bf6fa9f4
Bo Yu <by51@duke.edu>
2024-11-12 10:50:21 -0500

    Script to test simulation on the batch system

---
+++ sim/run_test_batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REACTION=piminus_p_2H
+WRAPPER_VERSION=gluex_MCwrapper_srcct-v2.10.1
+RUN=90213
+EVENTS=10000
+
+source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
+gxenv $HALLD_VERSIONS/version.xml
+export MCWRAPPER_CENTRAL=/work/halld2/home/boyu/src_software_builds/${WRAPPER_VERSION}
+export PATH=/work/halld2/home/boyu/src_software_builds/${WRAPPER_VERSION}:${PATH}
+
+gluex_MC.py configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} batch=2 per_file=1000000
