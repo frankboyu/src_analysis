93a96f7e47f536a5c7a868fffd433fb2f5765ee3
Bo Yu <by51@duke.edu>
2024-11-18 09:00:31 -0500

    Re-compile the dev version with coherent phi updates

--- sim/run_compile.sh
+++ sim/run_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-XML_VERSION=ver01_1.1
-SIM_VERSION=halld_sim_srcct-4.48.0^hdr438
+XML_VERSION=dev
+SIM_VERSION=halld_sim_srcct
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
