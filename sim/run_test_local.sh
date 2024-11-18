ddec84903695c5b89c5d24b5353ae2ccf777a4e6
Bo Yu <by51@duke.edu>
2024-11-18 08:46:15 -0500

    Change the test output directory

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -8,8 +8,7 @@ EVENTS=10000
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
 
-mkdir -p output/test
-cd output/test
+cd output
 
 gluex_MC.py ../../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} batch=0 per_file=1000000
 
