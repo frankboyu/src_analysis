7daba333df72a00caefe49875fbec1b7cee2c7de
Bo Yu <by51@duke.edu>
2024-11-19 11:50:46 -0500

    Change scripts to test the new coherent phi cross section model

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-REACTION=piminus_p_2H
+REACTION=phi_d_2H
 XML_VERSION=dev
 RUN=90213
-EVENTS=10000
+EVENTS=100000
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
