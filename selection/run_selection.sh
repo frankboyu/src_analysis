037f2c800dcf51efc7c3724bed9ffdec1b7c914e
Bo Yu <by51@duke.edu>
2024-12-03 15:38:33 -0500

    Optimize directory scheme for data output files

--- selection/run_selection.sh
+++ selection/run_selection.sh
@@ -5,5 +5,7 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
+mkdir output/${REACTION}
+
 python launch.py configs/jobs_root_analysis_${REACTION}.cfg 90000 90662
 swif2 run -workflow src_analysis_selection -maxconcurrent 1000
