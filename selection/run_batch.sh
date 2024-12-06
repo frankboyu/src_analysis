279c6aeac29d87e8b3044f1761605ca3baa2b569
Bo Yu <by51@duke.edu>
2024-12-06 16:27:17 -0500

    Edit DSelector run script so that the previous version is kept as backup

--- selection/run_batch.sh
+++ selection/run_batch.sh
@@ -5,7 +5,8 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-mkdir output/${REACTION}
+rm -r output/flattree_${REACTION}_backup
+mv output/flattree_${REACTION} output/flattree_${REACTION}_backup
 
 python launch.py configs/jobs_root_analysis_${REACTION}.cfg 90000 90662
 swif2 run -workflow src_analysis_selection -maxconcurrent 1000
