8e4d3c3419fce3efc1f94271c2d7530d1af44da8
Bo Yu <by51@duke.edu>
2024-12-30 15:15:02 -0500

    Rename the selection run scripts

--- selection/run_selection.sh
+++ selection/run_selection.sh
@@ -5,9 +5,5 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-rm -r output/flattree_${REACTION}_backup
-mv output/flattree_${REACTION} output/flattree_${REACTION}_backup
-mkdir output/flattree_${REACTION}
-
 python launch.py configs/jobs_root_analysis_${REACTION}.cfg 90000 90662
 swif2 run -workflow src_analysis_selection -maxconcurrent 1000
