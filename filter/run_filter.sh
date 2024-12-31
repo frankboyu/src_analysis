e86649af9bb6426ff25d909550a64fc88927c92f
Bo Yu <by51@duke.edu>
2024-12-31 09:12:11 -0500

    Edit run scripts of seletion and filter

--- filter/run_filter.sh
+++ filter/run_filter.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "Start time"
+date
 
-REACTION=$1
+CHANNEL=$1
+TAG=$2
+MODE=$3
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-root -b -q -l filters/filter_${REACTION}.C
+root -b -q -l "filters/filter_$CHANNEL.C(\"$TAG\", \"$MODE\")"
+
+echo "End time"
+date
--- selection/run_merge.sh
+++ selection/run_merge.sh
@@ -6,4 +6,7 @@ source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
 hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
+hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
+
 rm -r output/selectedhist_${REACTION}/
+rm -r output/selectedtree_${REACTION}/
