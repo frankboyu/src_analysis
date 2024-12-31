43de91d9fc4d1107bc02143bf27133aaee930894
Bo Yu <by51@duke.edu>
2024-12-31 13:55:35 -0500

    Edit the merge script of selection to distinguish between sim and data output files

--- selection/run_merge.sh
+++ selection/run_merge.sh
@@ -5,8 +5,13 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
-hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
+if [[ $REACTION == *"data"* ]]; then
+    hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/*.root
+    hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/*.root
+elif [[ $REACTION == *"sim"* ]]; then
+    hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
+    hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
+fi
 
 rm -r output/selectedhist_${REACTION}/
 rm -r output/selectedtree_${REACTION}/
