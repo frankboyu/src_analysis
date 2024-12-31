be7c9b389a3d517bd52ac869b1e8fccf87df3be7
Bo Yu <by51@duke.edu>
2024-12-31 15:22:48 -0500

    Run DSelector on test files with A-1 as the missing particle for all 3 targets

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_2H_ver01/root/tree_gd_pimprotmissprot__B4_F4_T1_S4_gen_MF/*90208_000.root
-TREENAME=gd_pimprotmissprot__B4_F4_T1_S4_Tree
+INPUTFILE=/work/halld2/home/boyu/src_analysis/skim/output/piminus_p_2H_test/tree_gd_pimprotinc__B4_F4_T1_S4/090213/*.root
+TREENAME=gd_pimprotinc__B4_F4_T1_S4_Tree
 SELECTOR=piminus_p_recon
-TAG=sim_2H_missprot
+TAG=data_2H_inc_test
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-rm -r output/selectedtree_${SELECTOR}_${TAG}_backup.root
-mv output/selectedtree_${SELECTOR}_${TAG}.root output/selectedtree_${SELECTOR}_${TAG}_backup.root
-
-cd output/
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../configs/DSelector_${SELECTOR}.C+'", '1')'
+cd output/test/
+root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C+'", '1')'
