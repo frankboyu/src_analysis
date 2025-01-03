0ec6d865db0dea950f30dc6919df787a4f589b37
Bo Yu <by51@duke.edu>
2025-01-03 15:41:19 -0500

    Test simulation with monotonic photon beam

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-REACTION=piminus_p_12C
-XML_VERSION=ver01_4.1
-RUN=90290
-EVENTS=100000
+REACTION=piminus_p_2H
+XML_VERSION=dev
+RUN=90213
+EVENTS=1000
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
