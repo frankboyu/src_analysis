9f959fbb173b7d770034abc7af1b9a2287df5257
Bo Yu <by51@duke.edu>
2025-01-13 14:09:06 -0500

    Add -f to hadd to enforce recreation of root file

--- selection/run_merge.sh
+++ selection/run_merge.sh
@@ -6,12 +6,12 @@ source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
 if [[ $REACTION == *"data"* ]]; then
-    hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/*.root
-    hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/*.root
+    hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/*.root
+    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/*.root
 elif [[ $REACTION == *"sim"* ]]; then
-    hadd output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
-    hadd output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
+    hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
+    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
 fi
 
-rm -r output/selectedhist_${REACTION}/
-rm -r output/selectedtree_${REACTION}/
+# rm -r output/selectedhist_${REACTION}/
+# rm -r output/selectedtree_${REACTION}/
