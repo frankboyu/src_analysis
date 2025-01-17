459c287d7687c1b120d5ac9bd1960c82f4f95082
Bo Yu <by51@duke.edu>
2025-01-16 20:39:11 -0500

    Add DSelector for piplus n channel, but still non-functional

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_12C_test/root/trees/tree_gc12_pimprotinc*.root
-TREENAME=gc12_pimprotinc__B4_F4_T1_S4_Tree
-SELECTOR=piminus_p_recon
+INPUTFILE=/work/halld2/home/boyu/src_analysis/skim/output/piminus_p_2H_test/tree_gd_pimprotmissprot__B4_F4_T1_S4/090213/*000.root
+TREENAME=gd_pimprotmissprot__B4_F4_T1_S4_Tree
+SELECTOR=piplus_n_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
