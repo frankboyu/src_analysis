45d0a64cd695871534dd50fdb6cc52f98a4874d7
Bo Yu <by51@duke.edu>
2025-01-20 14:08:28 -0500

    Test new phi_d simulation with updated generator

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-REACTION=piminus_p_2H
+REACTION=phi_d_2H
 XML_VERSION=dev
 RUN=90213
 EVENTS=1000
+OPTIONS='cleangenerate=0 cleangeant=0 cleanmcsmear=0'
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
 
 cd output
 
-gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} batch=0 per_file=1000000
+gluex_MC.py ../configs/wrapper_${REACTION}.cfg ${RUN} ${EVENTS} ${OPTIONS} batch=0 per_file=1000000
 
 rm -r 90*
