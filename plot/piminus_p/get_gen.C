10cda0697997030dd683fad8bf71b59467f9e5c2
Bo Yu <by51@duke.edu>
2025-01-30 14:38:59 -0500

    Rename script to get number of generated events

--- plot/piminus_p/get_gen.C
+++ plot/piminus_p/get_gen.C
@@ -1,5 +1,3 @@
-using namespace std;
-
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
@@ -22,6 +20,7 @@ using namespace std;
 #include <TMath.h>
 #include <TFitResult.h>
 
+using namespace std;
 
 int get_gen(string Reaction)
 {
@@ -40,12 +39,14 @@ int get_gen(string Reaction)
         input_name  = "/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_12C_ver04/root/generator/genOut_gen_MF_*.root";
 
     string txt_name = Form("output/yield_piminus_p_gen_%s.txt",Reaction.c_str());
-    string pdf_name = Form("output/plot_piminus_p_gen_%s.txt",Reaction.c_str());
+    string pdf_name = Form("output/plot_piminus_p_gen_%s.pdf",Reaction.c_str());
 
     TChain *input_chain = new TChain("genT");
     input_chain->Add(input_name.c_str());
     FILE *txt_file = fopen(txt_name.c_str(),"w");
     TCanvas *canvas = new TCanvas("canvas", "canvas", 800, 600);
+    gStyle->SetOptStat(1);
+    gStyle->SetOptFit(1);
 
     vector<double> energy_edges;
     vector<double> theta_edges;
@@ -80,7 +81,7 @@ int get_gen(string Reaction)
     {
         for (int j = 0; j < theta_bins; j++)
         {
-            histograms[i][j] = new TH1D(Form("hist_%d_%d", i, j), Form("Energy bin %f, Theta bin %f", energy_edges[i], theta_edges[j]), 80, 0.5, 1.3);
+            histograms[i][j] = new TH1D(Form("hist_%d_%d", i, j), Form("Energy bin %f, Theta bin %f", energy_edges[i], theta_edges[j]), 150, 0.0, 1.5);
         }
     }
     TLorentzVector *pBeam = new TLorentzVector();
@@ -88,7 +89,7 @@ int get_gen(string Reaction)
     TLorentzVector *pBaryon = new TLorentzVector();
     TLorentzVector pBeam_boost;
     TLorentzVector pMeson_boost;
-    double energy, theta, pminus;
+    double energy, theta, pminus, missmass;
 
     input_chain->SetBranchAddress("pBeam", &pBeam);
     input_chain->SetBranchAddress("pMeson", &pMeson);
@@ -105,6 +106,7 @@ int get_gen(string Reaction)
         pMeson_boost.Boost(boost);
         theta = pBeam_boost.Angle(pMeson_boost.Vect()) * 180 / TMath::Pi();
         pminus = (*pMeson + *pBaryon - *pBeam).Minus();
+        missmass = (*pMeson + *pBaryon - *pBeam).M();
 
         for (int j = 0; j < energy_bins; j++)
         {
@@ -114,7 +116,7 @@ int get_gen(string Reaction)
                 {
                     if (theta >= theta_edges[k] && theta < theta_edges[k+1])
                     {
-                        if ((*pMeson + *pBaryon - *pBeam).P() < 0.2 && (*pBaryon).P() > 0.2)
+                        if ((*pMeson + *pBaryon - *pBeam).P() < 0.20 && (*pBaryon).P() > 0.20)
                             {
                             yield[j][k] += 1;
                             histograms[j][k]->Fill(pminus);
@@ -127,16 +129,27 @@ int get_gen(string Reaction)
         }
     }
 
-
+    double this_yield;
     for (int i = 0; i < energy_bins; i++)
     {
         for (int j = 0; j < theta_bins; j++)
         {
-            fprintf(txt_file, "%3.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", energy_edges[i], energy_edges[i+1], theta_edges[j], theta_edges[j+1], yield[i][j]);
+            // this_yield = histograms[i][j]->Integral();
+
+            TF1 *model = new TF1("model", "[0]/sqrt(2*TMath::Pi()*[2]*[2])*exp(-0.5*((x-[1])/[2])**2)", 0.5, 1.3);
+            model->SetParameters(histograms[i][j]->GetMaximum()*0.9*histograms[i][j]->GetBinWidth(1), 0.9, 0.05);
+            model->SetParLimits(0, 0, histograms[i][j]->GetMaximum());
+            model->SetParLimits(1, 0.7, 1.1);
+            model->SetParLimits(2, 0.01, 0.20);
+            histograms[i][j]->Fit(model, "R");
+            this_yield = model->GetParameter(0)/histograms[i][j]->GetBinWidth(1);
             histograms[i][j]->Draw();
+            model->Draw("same");
+
             canvas->Update();
             canvas->Print((pdf_name+"(").c_str());
             canvas->Clear();
+            fprintf(txt_file, "%3.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", energy_edges[i], energy_edges[i+1], theta_edges[j], theta_edges[j+1], this_yield);
         }
     }
     canvas->Print((pdf_name+")").c_str());
