1d43663cccff7cb5d10f98d81b79ce407ab14bd9
Bo Yu <by51@duke.edu>
2025-02-04 08:14:55 -0500

    Add header file to define the constants used in filters

---
+++ filter/filters/const.h
@@ -0,0 +1,14 @@
+double mass_proton      = 0.93827208943;
+double mass_neutron     = 0.93956542052;
+double mass_piplus      = 0.13957039;
+double mass_piminus     = 0.13957039;
+double mass_pizero      = 0.1349768;
+double mass_kplus       = 0.493677;
+double mass_kminus      = 0.493677;
+double mass_phi         = 1.019461;
+double mass_rhozero     = 0.77526;
+
+double mass_deuteron    = 1.875612859;
+double mass_He4         = 3.727379238;
+
+double RadToDeg         = 180.0 / 3.14159265;
