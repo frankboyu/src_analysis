283cfadc83b6c1e327cdd9267590e2aac3f3e4b0
Bo Yu <by51@duke.edu>
2025-02-06 14:33:55 -0500

    Compile new halld_sim_srcct sub-version

--- sim/run_compile.sh
+++ sim/run_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-XML_VERSION=analysis-2021_11-ver10
-SIM_VERSION=halld_recon-4.51.0
+XML_VERSION=recon_srcct-2021_11-ver01_4.4
+SIM_VERSION=halld_sim_srcct-4.52.0.4^rec211111
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/${XML_VERSION}.xml
