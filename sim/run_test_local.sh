c3d52149ac0ee4990ef98b7d6f6ec13757c9f626
Bo Yu <by51@duke.edu>
2025-02-05 20:27:08 -0500

    Test JANA_CALIB_CONTEXT flag in simulation

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -3,7 +3,7 @@
 REACTION=phi_d_2H
 XML_VERSION=dev
 RUN=90213
-EVENTS=100
+EVENTS=10000
 OPTIONS='cleangenerate=0 cleangeant=0 cleanmcsmear=0'
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
