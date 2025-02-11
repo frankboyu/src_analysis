3f7307304ec20d9df6eb91c02ba0c7360236f155
Bo Yu <by51@duke.edu>
2025-02-11 15:03:29 -0500

    Update rho_d_recon DSelector and run on data

---
+++ selection/run_all_rho_d.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+start=`date +%s`
+
+# data
+sh run_batch.sh rho_d_recon_data_2H
+sh run_batch.sh rho_d_recon_data_4He
+sh run_batch.sh rho_d_recon_data_12C
+
+end=`date +%s`
+echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
