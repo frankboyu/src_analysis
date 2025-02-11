3502aa11b11f992a9420bb96b4cc767147cfbf76
Bo Yu <by51@duke.edu>
2025-02-11 09:19:49 -0500

    Test the thrown topology string

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,9 +2,9 @@
 
 start=`date +%s`
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_4He_ver03/root/thrown/*.root
-TREENAME=Thrown_Tree
-SELECTOR=piminus_p_thrown
+INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/piminus_p_2H_test/root/trees/tree_gd_pimprotmissprot__B4_F4_T1_S4_gen_MF_090213_000.root
+TREENAME=gd_pimprotmissprot__B4_F4_T1_S4_Tree
+SELECTOR=piminus_p_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
