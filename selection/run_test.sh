48fdbb307403a1d41488a22444caf506eef4de68
Bo Yu <by51@duke.edu>
2025-02-14 14:53:47 -0500

    Test piminus_p_recon DSelector to check the empty target background

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,9 +2,9 @@
 
 start=`date +%s`
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver10/tree_ghe_kpkmprotmisstri__B4_F4_T1_S4/merged/tree_ghe_kpkmprotmisstri__B4_F4_T1_S4_090061.root
-TREENAME=ghe_kpkmprotmisstri__B4_F4_T1_S4_Tree
-SELECTOR=phi_p_recon
+INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_gd_pimprotmissprot__B4_F4_T1_S4/merged/*.root
+TREENAME=gd_pimprotmissprot__B4_F4_T1_S4_Tree
+SELECTOR=piminus_p_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
