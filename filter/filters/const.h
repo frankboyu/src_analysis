1cc925c32ae9df68d8fbcbb82a5fb9fb3ba9d5dc
Bo Yu <by51@duke.edu>
2025-02-17 14:28:23 -0500

    Add more nuclear masses

--- filter/filters/const.h
+++ filter/filters/const.h
@@ -1,5 +1,3 @@
-double mass_proton      = 0.93827208943;
-double mass_neutron     = 0.93956542052;
 double mass_piplus      = 0.13957039;
 double mass_piminus     = 0.13957039;
 double mass_pizero      = 0.1349768;
@@ -8,7 +6,14 @@ double mass_kminus      = 0.493677;
 double mass_phi         = 1.019461;
 double mass_rhozero     = 0.77526;
 
-double mass_deuteron    = 1.875612859;
-double mass_He4         = 3.727379238;
+double mass_neutron     = 0.93956542052;
+double mass_proton      = 0.93827208943;
+double mass_2H          = 1.875612859;
+double mass_3H          = 2.808921004;
+double mass_3He         = 2.809413498;
+double mass_4He         = 3.727379238;
+double mass_11B         = 10.25510;
+double mass_11C         = 10.25708;
+double mass_12C         = 11.17793;
 
 double RadToDeg         = 180.0 / 3.14159265;
