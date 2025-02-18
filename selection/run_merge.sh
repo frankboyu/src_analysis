f6d8c5d1c4592f3b720b96e0ab83e68a9836ce75
Bo Yu <by51@duke.edu>
2025-02-18 13:00:43 -0500

    Also have a smaller selected tree file from one run for plotting filter histograms

--- selection/run_merge.sh
+++ selection/run_merge.sh
@@ -7,11 +7,25 @@ gxenv $HALLD_VERSIONS/version.xml
 
 if [[ $REACTION == *"data"* ]]; then
     hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/*.root
-    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/*.root
+    hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/*.root
+    if [[ $REACTION == *"2H"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090213.root
+    elif [[ $REACTION == *"4He"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090061.root
+    elif [[ $REACTION == *"12C"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090291.root
+    fi
 elif [[ $REACTION == *"sim"* ]]; then
     hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
-    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
+    hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/**/*.root
+    if [[ $REACTION == *"2H"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/090213/*.root
+    elif [[ $REACTION == *"4He"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/090061/*.root
+    elif [[ $REACTION == *"12C"* ]]; then
+        hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/090291/*.root
+    fi
 fi
 
-# rm -r output/selectedhist_${REACTION}/
-# rm -r output/selectedtree_${REACTION}/
+rm -r output/selectedhist_${REACTION}/
+rm -r output/selectedtree_${REACTION}/
