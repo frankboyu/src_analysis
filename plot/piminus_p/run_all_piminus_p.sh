288c11ead80ffe7b305104fb83999e4728e92436
Bo Yu <by51@duke.edu>
2025-02-28 10:43:40 -0500

    Add missing unknown reactions to the yield run script

--- plot/piminus_p/run_all_piminus_p.sh
+++ plot/piminus_p/run_all_piminus_p.sh
@@ -3,8 +3,10 @@
 start=`date +%s`
 
 REACTION_LIST=()
-# REACTION_LIST+=("recon_data_2H_missprot" "recon_data_4He_misshe3" "recon_data_12C_missb11")
-REACTION_LIST+=("recon_sim_2H_missprot_model" "recon_sim_4He_misshe3_model" "recon_sim_12C_missb11_model")
+REACTION_LIST+=("recon_data_2H_missprot" "recon_data_4He_misshe3" "recon_data_12C_missb11")
+REACTION_LIST+=("recon_data_2H_inc" "recon_data_4He_inc" "recon_data_12C_inc")
+# REACTION_LIST+=("recon_sim_2H_missprot_model" "recon_sim_4He_misshe3_model" "recon_sim_12C_missb11_model")
+# REACTION_LIST+=("recon_sim_2H_missprot_inc" "recon_sim_4He_misshe3_inc" "recon_sim_12C_missb11_inc")
 # REACTION_LIST+=("thrown_gen_2H_model" "thrown_gen_4He_model" "thrown_gen_12C_model")
 # REACTION_LIST+=("thrown_tagged_2H_model" "thrown_tagged_4He_model" "thrown_tagged_12C_model")
 
