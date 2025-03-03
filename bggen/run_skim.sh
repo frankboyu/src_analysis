154e51352c74b7d82c39ffa2e465ad59e152553b
Bo Yu <by51@duke.edu>
2025-03-03 15:25:09 -0500

    Update bggen skim config files and run them

--- bggen/run_skim.sh
+++ bggen/run_skim.sh
@@ -5,5 +5,5 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.21.0.xml
 
-python launch.py configs/jobs_analysis_bggen_${REACTION}.cfg 90034 90034
+python launch.py configs/jobs_analysis_bggen_${REACTION}.cfg 90001 90662
 swif2 run -workflow src_analysis_bggen -maxconcurrent 1000
