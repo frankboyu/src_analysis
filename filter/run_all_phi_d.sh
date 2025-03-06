168431baabf0df7d098c3ea16f34611ba7e5d415
Bo Yu <by51@duke.edu>
2025-03-06 11:57:40 -0500

    Add shell script to run filters for all phi_d reactions

---
+++ filter/run_all_phi_d.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+RUNMODE=$1
+INPUTMODE=$2
+OUTPUTMODE=$3
+
+start=`date +%s`
+
+REACTION_LIST=()
+REACTION_LIST+=("data_2H" "data_4He" "data_12C")
+
+if [ "$RUNMODE" == "local" ]; then
+    for REACTION in "${REACTION_LIST[@]}"
+    do
+        sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $INPUTMODE $OUTPUTMODE
+    done
+elif [ "$RUNMODE" == "batch" ]; then
+    for REACTION in "${REACTION_LIST[@]}"
+    do
+        JOB_WORKFLOW="-workflow src_analysis_filter"
+        JOB_NAME="-name phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d-%H-%M')"
+        JOB_RESOURCES="-account halld -partition production -os el9 -cores 1 -ram 4GB -disk 8GB -time 24hrs"
+        JOB_OUT="-stdout /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').out"
+        JOB_ERR="-stderr /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').err"
+        JOB_COMMAND="sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $INPUTMODE $OUTPUTMODE"
+        swif2 add-job $JOB_WORKFLOW $JOB_NAME $JOB_RESOURCES $JOB_OUT $JOB_ERR $JOB_COMMAND
+    done
+    swif2 run -workflow src_analysis_filter
+fi
+
+end=`date +%s`
+echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
