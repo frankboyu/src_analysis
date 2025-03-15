be4c7efff7e9efc45e3f75b797e8fbd9ab430838
Bo Yu <by51@duke.edu>
2025-03-15 14:09:31 -0400

    Check the simulation output from new phi_d simulation

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,9 +2,9 @@
 
 start=`date +%s`
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/bggen/output/4He_n_3739/tree_ghe_pimprotmisshe3__B4_F4_T1_S4/*.root
-TREENAME=ghe_pimprotmisshe3__B4_F4_T1_S4_Tree
-SELECTOR=piminus_p_recon
+INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_test/root/trees/tree_gd_kpkmd__B4_F4_gen_coherent_090213_000.root
+TREENAME=gd_kpkmd__B4_F4_Tree
+SELECTOR=phi_d_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.21.0.xml
