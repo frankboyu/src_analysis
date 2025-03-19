c7e24d5d648b61624aab68bea5bf95a099c5e960
Bo Yu <by51@duke.edu>
2025-03-19 14:54:01 -0400

    Add thrown files to piminus p filter run script

--- filter/run_all_piminus_p.sh
+++ filter/run_all_piminus_p.sh
@@ -5,20 +5,31 @@ OUTPUTMODE=$3
 
 start=`date +%s`
 
-REACTION_LIST=()
-# REACTION_LIST+=("data_2H_inc"      "data_2H_missprot"       "data_4He_inc"      "data_4He_misshe3"      "data_12C_inc"      "data_12C_missb11")
-REACTION_LIST+=("sim_2H_inc_flat"  "sim_2H_missprot_flat"   "sim_4He_inc_flat"  "sim_4He_misshe3_flat"  "sim_12C_inc_flat"  "sim_12C_missb11_flat")
-REACTION_LIST+=("sim_2H_inc_model" "sim_2H_missprot_model"  "sim_4He_inc_model" "sim_4He_misshe3_model" "sim_12C_inc_model" "sim_12C_missb11_model")
-# REACTION_LIST+=("bggen_4He_n_inc"   "bggen_4He_n_misshe3"   "bggen_4He_p_inc"   "bggen_4He_p_misshe3")
-# REACTION_LIST+=("bggen_12C_n_inc"   "bggen_12C_n_missb11"   "bggen_12C_p_inc"   "bggen_12C_p_missb11")
+RECON_LIST=()
+# RECON_LIST+=("data_2H_inc"      "data_2H_missprot"       "data_4He_inc"      "data_4He_misshe3"      "data_12C_inc"      "data_12C_missb11")
+# RECON_LIST+=("sim_2H_inc_flat"  "sim_2H_missprot_flat"   "sim_4He_inc_flat"  "sim_4He_misshe3_flat"  "sim_12C_inc_flat"  "sim_12C_missb11_flat")
+# RECON_LIST+=("sim_2H_inc_model" "sim_2H_missprot_model"  "sim_4He_inc_model" "sim_4He_misshe3_model" "sim_12C_inc_model" "sim_12C_missb11_model")
+# RECON_LIST+=("bggen_4He_n_inc"   "bggen_4He_n_misshe3"   "bggen_4He_p_inc"   "bggen_4He_p_misshe3")
+# RECON_LIST+=("bggen_12C_n_inc"   "bggen_12C_n_missb11"   "bggen_12C_p_inc"   "bggen_12C_p_missb11")
+
+THROWN_LIST=()
+# THROWN_LIST+=("gen_2H_model"        "gen_4He_model"     "gen_12C_model")
+THROWN_LIST+=("gen_2H_flat"       "gen_4He_flat"      "gen_12C_flat")
+# THROWN_LIST+=("tagged_2H_model"   "tagged_4He_model"  "tagged_12C_model")
+THROWN_LIST+=("tagged_2H_flat"    "tagged_4He_flat"   "tagged_12C_flat")
 
 if [ "$RUNMODE" == "local" ]; then
-    for REACTION in "${REACTION_LIST[@]}"
+    for REACTION in "${RECON_LIST[@]}"
     do
         sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh piminus_p_recon $REACTION $INPUTMODE $OUTPUTMODE
     done
+
+    for REACTION in "${THROWN_LIST[@]}"
+    do
+        sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh piminus_p_thrown $REACTION $INPUTMODE $OUTPUTMODE
+    done
 elif [ "$RUNMODE" == "batch" ]; then
-    for REACTION in "${REACTION_LIST[@]}"
+    for REACTION in "${RECON_LIST[@]}"
     do
         JOB_WORKFLOW="-workflow src_analysis_filter"
         JOB_NAME="-name piminus_p_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d-%H-%M')"
@@ -26,6 +37,18 @@ elif [ "$RUNMODE" == "batch" ]; then
         JOB_OUT="-stdout /farm_out/boyu/src_analysis/filter/piminus_p_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').out"
         JOB_ERR="-stderr /farm_out/boyu/src_analysis/filter/piminus_p_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').err"
         JOB_COMMAND="sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh piminus_p_recon $REACTION $INPUTMODE $OUTPUTMODE"
+        swif2 add-job $JOB_WORKFLOW $JOB_NAME $JOB_RESOURCES $JOB_OUT $JOB_ERR $JOB_COMMAND
+    done
+    swif2 run -workflow src_analysis_filter
+
+    for REACTION in "${THROWN_LIST[@]}"
+    do
+        JOB_WORKFLOW="-workflow src_analysis_filter"
+        JOB_NAME="-name piminus_p_thrown_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d-%H-%M')"
+        JOB_RESOURCES="-account halld -partition production -os el9 -cores 1 -ram 4GB -disk 8GB -time 24hrs"
+        JOB_OUT="-stdout /farm_out/boyu/src_analysis/filter/piminus_p_thrown_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').out"
+        JOB_ERR="-stderr /farm_out/boyu/src_analysis/filter/piminus_p_thrown_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').err"
+        JOB_COMMAND="sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh piminus_p_thrown $REACTION $INPUTMODE $OUTPUTMODE"
         swif2 add-job $JOB_WORKFLOW $JOB_NAME $JOB_RESOURCES $JOB_OUT $JOB_ERR $JOB_COMMAND
     done
     swif2 run -workflow src_analysis_filter
