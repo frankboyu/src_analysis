c9b4682de05692f20463da074dd62b5da92d7309
Bo Yu <by51@duke.edu>
2025-03-23 14:26:01 -0400

    Revert selection merge script

--- selection/run_merge.sh
+++ selection/run_merge.sh
@@ -5,29 +5,12 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.21.0.xml
 
-if [[ $REACTION == *"data"* ]]; then
-    hadd -f output/selectedhist_${REACTION}_all.root output/selectedhist_${REACTION}/*.root
-    hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/*.root
-    if [[ $REACTION == *"2H"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090213.root
-    elif [[ $REACTION == *"4He"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090061.root
-    elif [[ $REACTION == *"12C"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/*090291.root
-    fi
+if [[ $REACTION == *"data"* || $REACTION == *"bggen"*]]; then
+    hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/*.root
+    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/*.root
 elif [[ $REACTION == *"sim"* ]]; then
-    hadd -f output/selectedhist_${REACTION}_all.root output/selectedhist_${REACTION}/**/*.root
-    hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/**/*.root
-    if [[ $REACTION == *"2H"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/090213/*.root
-    elif [[ $REACTION == *"4He"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/090061/*.root
-    elif [[ $REACTION == *"12C"* ]]; then
-        hadd -f output/selectedtree_${REACTION}_one.root output/selectedtree_${REACTION}/090291/*.root
-    fi
-elif [[ $REACTION == *"bggen"* ]]; then
-    hadd -f output/selectedhist_${REACTION}_all.root output/selectedhist_${REACTION}/*.root
-    hadd -f output/selectedtree_${REACTION}_all.root output/selectedtree_${REACTION}/*.root
+    hadd -f output/selectedhist_${REACTION}.root output/selectedhist_${REACTION}/**/*.root
+    hadd -f output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}/**/*.root
 fi
 
 rm -r output/selectedhist_${REACTION}/
