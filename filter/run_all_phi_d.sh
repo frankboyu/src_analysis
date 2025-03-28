7efed9229a5e986836528551a3d2b135405b0e64
Bo Yu <by51@duke.edu>
2025-03-28 10:53:53 -0400

    Remove input mode argument from the filter run scripts

--- filter/run_all_phi_d.sh
+++ filter/run_all_phi_d.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 RUNMODE=$1
-INPUTMODE=$2
-OUTPUTMODE=$3
+OUTPUTMODE=$2
 
 start=`date +%s`
 
@@ -11,17 +10,17 @@ REACTION_LIST+=("data_2H" "data_4He" "data_12C")
 if [ "$RUNMODE" == "local" ]; then
     for REACTION in "${REACTION_LIST[@]}"
     do
-        sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $INPUTMODE $OUTPUTMODE
+        sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $OUTPUTMODE
     done
 elif [ "$RUNMODE" == "batch" ]; then
     for REACTION in "${REACTION_LIST[@]}"
     do
         JOB_WORKFLOW="-workflow src_analysis_filter"
-        JOB_NAME="-name phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d-%H-%M')"
+        JOB_NAME="-name phi_d_recon_${REACTION}_${OUTPUTMODE}_$(date '+%Y-%m-%d-%H-%M')"
         JOB_RESOURCES="-account halld -partition production -os el9 -cores 1 -ram 4GB -disk 8GB -time 24hrs"
-        JOB_OUT="-stdout /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').out"
-        JOB_ERR="-stderr /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${INPUTMODE}_${OUTPUTMODE}_$(date '+%Y-%m-%d').err"
-        JOB_COMMAND="sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $INPUTMODE $OUTPUTMODE"
+        JOB_OUT="-stdout /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${OUTPUTMODE}_$(date '+%Y-%m-%d').out"
+        JOB_ERR="-stderr /farm_out/boyu/src_analysis/filter/phi_d_recon_${REACTION}_${OUTPUTMODE}_$(date '+%Y-%m-%d').err"
+        JOB_COMMAND="sh /work/halld2/home/boyu/src_analysis/filter/run_filter.sh phi_d_recon $REACTION $OUTPUTMODE"
         swif2 add-job $JOB_WORKFLOW $JOB_NAME $JOB_RESOURCES $JOB_OUT $JOB_ERR $JOB_COMMAND
     done
     swif2 run -workflow src_analysis_filter
--- filter/run_filter.sh
+++ filter/run_filter.sh
@@ -4,13 +4,12 @@ start=`date +%s`
 
 CHANNEL=$1
 REACTION=$2
-INPUTMODE=$3
-OUTPUTMODE=$4
+OUTPUTMODE=$3
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-root -b -q -l "/work/halld2/home/boyu/src_analysis/filter/configs/filter_$CHANNEL.C(\"$REACTION\", \"$INPUTMODE\", \"$OUTPUTMODE\")"
+root -b -q -l "/work/halld2/home/boyu/src_analysis/filter/configs/filter_$CHANNEL.C(\"$REACTION\", \"$OUTPUTMODE\")"
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
