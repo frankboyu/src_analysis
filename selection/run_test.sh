2b81fd04421773d12b5b82f083197c91d64d382a
Bo Yu <by51@duke.edu>
2025-04-15 09:45:41 -0400

    Test updated piminus_p_recon DSelector on bggen files

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,9 +2,9 @@
 
 start=`date +%s`
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/rho_d_2H_test/root/trees/tree_gd_pippimd__B4_F4_gen_coherent_090213_000.root
-TREENAME=gd_pippimd__B4_F4_Tree
-SELECTOR=rho_d_recon
+INPUTFILE=/work/halld2/home/boyu/src_analysis/bggen/output/4He_p_ver02/tree_ghe_pimprotmisshe3__B4_F4_T1_S4_bggen/*90090.root
+TREENAME=ghe_pimprotmisshe3__B4_F4_T1_S4_bggen_Tree
+SELECTOR=piminus_p_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.21.0.xml
