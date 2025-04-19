5bf18b7f937a3285451ebd404ab8dba3dc7d81ea
Bo Yu <by51@duke.edu>
2025-04-19 16:14:38 -0400

    Edit phi_d data config files to test reactions with and without missing particle

--- data/run_test_batch.sh
+++ data/run_test_batch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-REACTION=piplus_n_2H
-RUN=90213
+REACTION=phi_d_2H
+RUNMIN=90211
+RUNMAX=90211
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-python launch.py configs/jobs_analysis_${REACTION}.cfg ${RUN} ${RUN}
+python launch.py configs/jobs_analysis_${REACTION}.cfg ${RUNMIN} ${RUNMAX}
 swif2 run -workflow src_analysis_data
