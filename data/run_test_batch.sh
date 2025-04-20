6c183cc8969c36040e68dfb43a3d00f16651b5d1
Bo Yu <by51@duke.edu>
2025-04-20 11:19:46 -0400

    Test phi_d_12C data with and without missing particle

--- data/run_test_batch.sh
+++ data/run_test_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REACTION=phi_d_4He
-RUNMIN=90101
-RUNMAX=90105
+REACTION=phi_d_12C
+RUNMIN=90291
+RUNMAX=90295
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
