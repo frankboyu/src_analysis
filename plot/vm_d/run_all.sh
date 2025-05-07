67d7a61084dd3bdc43afbfb785f33015ee69c88f
Bo Yu <by51@duke.edu>
2025-05-07 13:53:48 -0400

    Add channel list and observable list to the run script

--- plot/vm_d/run_all.sh
+++ plot/vm_d/run_all.sh
@@ -2,14 +2,26 @@
 
 start=`date +%s`
 
+CHANNEL_LIST=()
+CHANNEL_LIST+=("phi_d")
+
 REACTION_LIST=()
 REACTION_LIST+=("recon_data_2H" "recon_sim_2H" "thrown_tagged_2H")
 
+OBSERVABLE_LIST=()
+OBSERVABLE_LIST+=("ds_dt" "W_costheta" "W_phi")
+
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
-for REACTION in "${REACTION_LIST[@]}"
+for OBSERVABLE in "${OBSERVABLE_LIST[@]}"
 do
-    root -b -q -l "get_yield.C(\"$REACTION\")"
+    for CHANNEL in "${CHANNEL_LIST[@]}"
+    do
+        for REACTION in "${REACTION_LIST[@]}"
+        do
+            root -b -q -l "get_yield.C(\"$CHANNEL\", \"$REACTION\", \"$OBSERVABLE\")"
+        done
+    done
 done
 
 end=`date +%s`
