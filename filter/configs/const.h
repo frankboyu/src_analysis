a8d03f3aa3ce1fd45450e8309a8f6429c73b1970
Bo Yu <by51@duke.edu>
2025-05-08 13:24:25 -0400

    Add const qualifiers to variables

--- filter/configs/const.h
+++ filter/configs/const.h
@@ -12,25 +12,25 @@ using namespace RooFit;
 using namespace ROOT::RDF;
 using namespace ROOT::Detail::RDF;
 
-double mass_piplus      = 0.13957039;
-double mass_piminus     = 0.13957039;
-double mass_pi0         = 0.1349768;
-double mass_kplus       = 0.493677;
-double mass_kminus      = 0.493677;
-double mass_phi         = 1.019461;
-double mass_rho0        = 0.77526;
+const double mass_piplus      = 0.13957039;
+const double mass_piminus     = 0.13957039;
+const double mass_pi0         = 0.1349768;
+const double mass_kplus       = 0.493677;
+const double mass_kminus      = 0.493677;
+const double mass_phi         = 1.019461;
+const double mass_rho0        = 0.77526;
 
-double mass_neutron     = 0.93956542052;
-double mass_proton      = 0.93827208943;
-double mass_2H          = 1.875612859;
-double mass_3H          = 2.808921004;
-double mass_3He         = 2.809413498;
-double mass_4He         = 3.727379238;
-double mass_11B         = 10.25510;
-double mass_11C         = 10.25708;
-double mass_12C         = 11.17793;
+const double mass_neutron     = 0.93956542052;
+const double mass_proton      = 0.93827208943;
+const double mass_2H          = 1.875612859;
+const double mass_3H          = 2.808921004;
+const double mass_3He         = 2.809413498;
+const double mass_4He         = 3.727379238;
+const double mass_11B         = 10.25510;
+const double mass_11C         = 10.25708;
+const double mass_12C         = 11.17793;
 
-double RadToDeg         = 180.0 / 3.14159265;
+const double RadToDeg         = 180.0 / 3.14159265;
 
 TLorentzVector boost_lorentz_vector(TLorentzVector p4, TVector3 boost_vector)
 {
