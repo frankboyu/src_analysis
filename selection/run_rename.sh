477e7138b471dae72afcf188f503791dd732590b
Bo Yu <by51@duke.edu>
2025-05-13 16:01:46 -0400

    Correct typo

--- selection/run_rename.sh
+++ selection/run_rename.sh
@@ -5,10 +5,10 @@ TAG=$2
 SUBTAG=$3
 
 if [[ $REACTION == *"recon"* ]]; then
-    echo mv output/selectedhist_${REACTION}_${TAG}.root output/selectedhist_${REACTION}_${TAG}_${SUBTAG}.root
-    echo mv output/selectedtree_${REACTION}_${TAG}.root output/selectedtree_${REACTION}_${TAG}_${SUBTAG}.root
+    mv output/selectedhist_${REACTION}_${TAG}.root output/selectedhist_${REACTION}_${TAG}_${SUBTAG}.root
+    mv output/selectedtree_${REACTION}_${TAG}.root output/selectedtree_${REACTION}_${TAG}_${SUBTAG}.root
 elif [[ $REACTION == *"thrown"* ]]; then
-    echo mv output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}_${SUBTAG}.root
+    mv output/selectedtree_${REACTION}.root output/selectedtree_${REACTION}_${SUBTAG}.root
 else
     echo "Invalid REACTION type. Please use 'recon' or 'thrown'."
     exit 1
