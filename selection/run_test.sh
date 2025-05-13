8a746e4f81d675ca3d7469ebf71a4fd42d05163b
Bo Yu <by51@duke.edu>
2025-05-13 13:22:14 -0400

    Test updated phi_d DSelector before mass production

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,15 +2,15 @@
 
 start=`date +%s`
 
-INPUTFILE=/work/halld2/home/boyu/src_analysis/test/tree_gd_kpkmd__B4_F4.root
+INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver10/tree_gd_kpkmd__B4_F4/merged/tree_gd_kpkmd__B4_F4_090208.root
 TREENAME=gd_kpkmd__B4_F4_Tree
 SELECTOR=phi_d_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_6.0.0.xml
+gxenv $HALLD_VERSIONS/version_5.21.0.xml
 
 cd output/test/
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C+'", '1')'
+root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C++'", '1')'
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
