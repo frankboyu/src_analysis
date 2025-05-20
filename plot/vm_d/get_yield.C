8e556d900291f3be1bc82e155266529f33184704
Bo Yu <by51@duke.edu>
2025-05-20 14:46:57 -0400

    Rename variables in vm_d yield script. Add code to get yield in terms of Phi and psi angles in yield script and run script

--- plot/vm_d/get_yield.C
+++ plot/vm_d/get_yield.C
@@ -8,16 +8,16 @@ using namespace std;
 int get_yield(string channel, string reaction, string observable)
 {
     // Read tree from input root file
-    string input_root_name  = Form("/work/halld2/home/boyu/src_analysis/filter/output/filteredtree_%s_%s.root", channel.c_str(), reaction.c_str());
-    cout << "Input root file: " << input_root_name << endl;
-    TFile *input_root = new TFile(input_root_name.c_str(), "read");
+    string input_treefile_name  = Form("/work/halld2/home/boyu/src_analysis/filter/output/filteredtree_%s_%s.root", channel.c_str(), reaction.c_str());
+    cout << "Input tree file: " << input_treefile_name << endl;
+    TFile *input_treefile = new TFile(input_treefile_name.c_str(), "read");
     string input_tree_name;
     if (reaction.find("recon") != string::npos)
         input_tree_name = Form("filteredtree_%s_recon", channel.c_str());
     else if (reaction.find("thrown") != string::npos)
         input_tree_name = Form("filteredtree_%s_thrown", channel.c_str());
-    cout << "Input tree name: " << input_tree_name << endl;
-    TTree *input_tree = (TTree*) input_root->Get(input_tree_name.c_str());
+    cout << "Input tree: " << input_tree_name << endl;
+    TTree *input_tree = (TTree*) input_treefile->Get(input_tree_name.c_str());
 
     // Read bin edges from input text file
     string target;
@@ -28,7 +28,7 @@ int get_yield(string channel, string reaction, string observable)
     else if (reaction.find("12C") != string::npos)
         target = "12C";
     string input_txt_name = Form("output/bins_%s_%s_%s.txt", channel.c_str(), target.c_str(), observable.c_str());
-    cout << "Input bin file: " << input_txt_name << endl;
+    cout << "Input text file: " << input_txt_name << endl;
     ifstream input_txt(input_txt_name.c_str());
     vector<vector<double>> bins;
     string this_line;
@@ -44,11 +44,11 @@ int get_yield(string channel, string reaction, string observable)
     input_txt.close();
 
     // Prepare output files
-    string output_txt_name = Form("output/yield_%s_%s_%s.txt", channel.c_str(), reaction.c_str(), observable.c_str());
-    string output_pdf_name = Form("output/plot_%s_%s_%s.pdf", channel.c_str(), reaction.c_str(), observable.c_str());
-    cout << "Output text file: " << output_txt_name << endl;
-    cout << "Output pdf file: " << output_pdf_name << endl;
-    FILE *txt_file = fopen(output_txt_name.c_str(),"w");
+    string output_textfile_name = Form("output/yield_%s_%s_%s.txt", channel.c_str(), reaction.c_str(), observable.c_str());
+    string output_pdffile_name = Form("output/plot_%s_%s_%s.pdf", channel.c_str(), reaction.c_str(), observable.c_str());
+    cout << "Output text file: " << output_textfile_name << endl;
+    cout << "Output pdf file: " << output_pdffile_name << endl;
+    FILE *output_textfile = fopen(output_textfile_name.c_str(),"w");
 
     // Declare variables used for the yield calculation
     string yield_observable;
@@ -106,9 +106,9 @@ int get_yield(string channel, string reaction, string observable)
             cout << "cos(theta_H): " << bins[i][4] << "-" << bins[i][5] << endl;
             this_hist_name = Form("hist_%.1f_%.1f_%.1f_%.1f_%.1f_%.1f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
             if (reaction.find("recon") != string::npos)
-                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && costheta_helicity_kin>%f && costheta_helicity_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && kp_costheta_helicity_kin>%f && kp_costheta_helicity_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
             else if (reaction.find("thrown") != string::npos)
-                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && costheta_helicity_truth>%f && costheta_helicity_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && kp_costheta_helicity_truth>%f && kp_costheta_helicity_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
         }
         else if (observable == "Wphi")
         {
@@ -117,25 +117,47 @@ int get_yield(string channel, string reaction, string observable)
             cout << "phi: " << bins[i][4] << "-" << bins[i][5] << " deg" << endl;
             this_hist_name = Form("hist_%.1f_%.1f_%.1f_%.1f_%.1f_%.1f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
             if (reaction.find("recon") != string::npos)
-                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && phi_helicity_kin>%f && phi_helicity_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && kp_phi_helicity_kin>%f && kp_phi_helicity_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
             else if (reaction.find("thrown") != string::npos)
-                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && phi_helicity_truth>%f && phi_helicity_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && kp_phi_helicity_truth>%f && kp_phi_helicity_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+        }
+        else if (observable == "WPhi")
+        {
+            cout << "Photon energy: " << bins[i][0] << "-" << bins[i][1] << " GeV" << ", ";
+            cout << "-t: " << bins[i][2] << "-" << bins[i][3] << " GeV^2" << ", ";
+            cout << "Phi: " << bins[i][4] << "-" << bins[i][5] << " deg" << endl;
+            this_hist_name = Form("hist_%.1f_%.1f_%.1f_%.1f_%.1f_%.1f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+            if (reaction.find("recon") != string::npos)
+                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && polarization_phi_com_kin>%f && polarization_phi_com_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+            else if (reaction.find("thrown") != string::npos)
+                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && polarization_phi_com_truth>%f && polarization_phi_com_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+        }
+        else if (observable == "Wpsi")
+        {
+            cout << "Photon energy: " << bins[i][0] << "-" << bins[i][1] << " GeV" << ", ";
+            cout << "-t: " << bins[i][2] << "-" << bins[i][3] << " GeV^2" << ", ";
+            cout << "psi: " << bins[i][4] << "-" << bins[i][5] << " deg" << endl;
+            this_hist_name = Form("hist_%.1f_%.1f_%.1f_%.1f_%.1f_%.1f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+            if (reaction.find("recon") != string::npos)
+                this_hist_cut = Form("accidweight*(beam_energy_kin>%f && beam_energy_kin<%f && minust_kin>%f && minust_kin<%f && psi_helicity_kin>%f && psi_helicity_kin<%f)", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
+            else if (reaction.find("thrown") != string::npos)
+                this_hist_cut = Form("beam_energy_truth>%f && beam_energy_truth<%f && minust_truth>%f && minust_truth<%f && psi_helicity_truth>%f && psi_helicity_truth<%f", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5]);
         }
 
         this_hist = new TH1F(this_hist_name.c_str(), this_hist_name.c_str(), hist_bins, hist_min, hist_max);
         input_tree->Draw(Form("%s>>%s", yield_observable.c_str(), this_hist_name.c_str()), this_hist_cut.c_str());  // draw the yield observable into the histogram with the cut
         this_yield = this_hist->Integral();
         if (observable == "dsdt")
-            fprintf(txt_file, "%6.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", bins[i][0], bins[i][1], bins[i][2], bins[i][3], this_yield);
-        else if (observable == "Wcostheta" || observable == "Wphi")
-            fprintf(txt_file, "%6.1f\t%6.1f\t%6.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5], this_yield);
+            fprintf(output_textfile, "%6.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", bins[i][0], bins[i][1], bins[i][2], bins[i][3], this_yield);
+        else if (observable == "Wcostheta" || observable == "Wphi" || observable == "WPhi" || observable == "Wpsi")
+            fprintf(output_textfile, "%6.1f\t%6.1f\t%6.1f\t%6.1f\t%6.1f\t%6.1f\t%f\n", bins[i][0], bins[i][1], bins[i][2], bins[i][3], bins[i][4], bins[i][5], this_yield);
         this_hist->Draw();
         canvas->Update();
-        canvas->Print((output_pdf_name+"(").c_str());
+        canvas->Print((output_pdffile_name+"(").c_str());
         canvas->Clear();
         delete this_hist;
     }
-    canvas->Print((output_pdf_name+")").c_str());  // close the pdf file
+    canvas->Print((output_pdffile_name+")").c_str());  // close the pdf file
 
     return 0;
 
