bf2e4f00cedaf801b44017a5c4f8f52570b00264
Bo Yu <by51@duke.edu>
2025-06-21 15:07:15 -0400

    Compile halld_recon_srcct with JANA2

--- sim/run_compile.sh
+++ sim/run_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-XML_VERSION=analysis-2021_11-ver10.2-CentOS7
-SIM_VERSION=halld_recon_srcct-4.51.0.2^CentOS7
+XML_VERSION=analysis-2021_11-ver11-CentOS7
+SIM_VERSION=halld_recon_srcct-5.3.0.1^CentOS7
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/${XML_VERSION}.xml
