8f16b1b2e1916de77a88a6f976f6ab56c259d323
Bo Yu <by51@duke.edu>
2025-06-24 11:14:49 -0400

    Test SC hits on bggen files

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,16 +2,15 @@
 
 start=`date +%s`
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver06/tree_ghe_pippimprotinc__B4_F4_T2_S5/merged/tree_ghe_pippimprotinc__B4_F4_T2_S5_090661.root
-TREENAME=ghe_pippimprotinc__B4_F4_T2_S5_Tree
-SELECTOR=1p_0T_inc
+INPUTFILE=/work/halld2/home/boyu/src_analysis/test/piminus_missprot.root
+TREENAME=gd_pimprotmissprot__B4_F4_T1_S4_Tree
+SELECTOR=piminus_p_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version_5.21.0.xml
-echo gxenv $HALLD_VERSIONS/version_5.21.0.xml
 
 cd output/test/
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DS_${SELECTOR}.C++'", '1')'
+root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C++'", '1')'
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
