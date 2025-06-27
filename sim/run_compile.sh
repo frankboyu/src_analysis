724cfc8bffb7046e7eb17a2b09c08967ddf2a3ab
Bo Yu <by51@duke.edu>
2025-06-27 11:20:33 -0400

    Compile, test, run and log phi_d_2H sim ver02

--- sim/run_compile.sh
+++ sim/run_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-XML_VERSION=analysis-2021_11-ver11-CentOS7
-SIM_VERSION=halld_recon_srcct-5.3.0.1^CentOS7
+XML_VERSION=recon_srcct-2021_11-ver01_4.7
+SIM_VERSION=halld_sim_srcct-4.52.0.7^rec211111
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/${XML_VERSION}.xml
