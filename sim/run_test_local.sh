5c4f65148d6f111cd84b48da81f3f770b2473ca5
Bo Yu <by51@duke.edu>
2025-07-08 17:09:03 -0400

    Rerun run 90213 for phi_d_2H ver01 simulation to make up for the accidental overwriting

--- sim/run_test_local.sh
+++ sim/run_test_local.sh
@@ -3,8 +3,9 @@
 REACTION=phi_d_2H
 XML_VERSION=ver01_4.5
 RUN=90213
-EVENTS=10000
-OPTIONS='cleangenerate=0 cleangeant=0 cleanmcsmear=0'
+EVENTS=172949
+# OPTIONS='cleangenerate=0 cleangeant=0 cleanmcsmear=0'
+OPTIONS=''
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/recon_srcct-2021_11-${XML_VERSION}.xml
