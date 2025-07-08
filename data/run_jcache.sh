fed22474fdb2b4b06c14d2399228e5e6789e07fa
Bo Yu <by51@duke.edu>
2025-07-08 13:51:34 -0400

    Remake the script to use jcache to get data files from tape

--- data/run_jcache.sh
+++ data/run_jcache.sh
@@ -1,3 +1,31 @@
-FILE=/mss/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm
+#!/bin/bash
 
-jcache get ${FILE}
+REACTION=$1
+PINDAYS=$2
+
+DIR_LIST=()
+
+if   [ "${REACTION}" == "phi_d_2H_ver10" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver10/tree_gd_kpkmd__B4_F4/merged")
+elif [ "${REACTION}" == "phi_d_2H_ver11" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_kpkmd__B4_F4/merged")
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_kpkmdinc__B4_F4_T1_S2/merged")
+elif [ "${REACTION}" == "rho_d_2H_ver10" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver10/tree_gd_pippimd__B4_F4/merged")
+elif [ "${REACTION}" == "rho_d_2H_ver11" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pippimd__B4_F4/merged")
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pippimdinc__B4_F4_T1_S2/merged")
+elif [ "${REACTION}" == "omega_d_2H_ver11" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pi0pippimd__B4_F4/merged")
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pi0pippimdinc__B4_F4_T1_S2/merged")
+fi
+
+for DIR in "${DIR_LIST[@]}"
+do
+    jcache get ${DIR}/*.root -D ${PINDAYS}
+done
