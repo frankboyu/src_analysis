4d1f25cb303559bec7d009e79724b51cfa75a8f7
Bo Yu <by51@duke.edu>
2025-07-09 20:21:12 -0400

    Have data/sim version based decision for running verison in the DSelector local run script, in order to keep software compatibility

--- selection/run_local.sh
+++ selection/run_local.sh
@@ -7,15 +7,26 @@ FILENAME=$2
 TREENAME=$3
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-if [[ $REACTION == *"recon"* ]]; then
-    gxenv $HALLD_VERSIONS/version_5.21.0.xml
-elif [[ $REACTION == *"thrown"* ]]; then
-    gxenv $HALLD_VERSIONS/version.xml
+if [[ $FILENAME == *"data"* ]]; then
+    if [[ $FILENAME == *"ver10"* ]]; then
+        echo gxenv $HALLD_VERSIONS/version_5.21.0.xml
+        gxenv $HALLD_VERSIONS/version_5.21.0.xml
+    elif [[ $FILENAME == *"ver11"* ]]; then
+        echo gxenv $HALLD_VERSIONS/version_6.3.0.xml
+        gxenv $HALLD_VERSIONS/version_6.3.0.xml
+    fi
+elif [[ $FILENAME == *"sim"* ]]; then
+    if [[ $FILENAME == *"ver01"* || $FILENAME == *"ver02"* ]]; then
+        echo gxenv $HALLD_VERSIONS/version_5.21.0.xml
+        gxenv $HALLD_VERSIONS/version_5.21.0.xml
+    elif [[ $FILENAME == *"ver03"* ]]; then
+        echo gxenv $HALLD_VERSIONS/version_6.3.0.xml
+        gxenv $HALLD_VERSIONS/version_6.3.0.xml
+    fi
 fi
 
-
 cd output/
-root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../process_chain.C'("'$FILENAME'", "'$TREENAME'", "'../configs/DSelector_${REACTION}.C+'", '1')'
+root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../process_chain.C'("'$FILENAME'", "'$TREENAME'", "'../configs/DSelector_${REACTION}.C++'", '1')'
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
