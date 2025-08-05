a5a1425451087883cc6d0269e4a5f7fa9c77e4f7
Bo Yu <by51@duke.edu>
2025-08-05 14:18:12 -0400

    Test run vm_d skim on recon ver03 files

--- data/run_test_batch.sh
+++ data/run_test_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REACTION=phi_d_12C
-RUNMIN=90291
-RUNMAX=90295
+REACTION=phi_d_2H
+RUNMIN=90211
+RUNMAX=90215
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
