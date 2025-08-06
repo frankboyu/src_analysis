b2c154593edf952e43af3e0237be79ea7f98c54e
Bo Yu <by51@duke.edu>
2025-08-06 14:41:30 -0400

    Test phi_d reaction with recon ver03 files, turns out wrong PID was used for deuteron

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,13 +2,13 @@
 
 start=`date +%s`
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/analysis/ver10/tree_gd_kpkmd__B4_F4/merged/tree_gd_kpkmd__B4_F4_090213.root
+INPUTFILE=/work/halld2/home/boyu/src_analysis/data/output/phi_d_2H_test/tree_gd_kpkmd__B4_F4/*.root
 TREENAME=gd_kpkmd__B4_F4_Tree
 SELECTOR=phi_d_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.21.0.xml
-# gxenv $HALLD_VERSIONS/version.xml
+# gxenv $HALLD_VERSIONS/version_5.21.0.xml
+gxenv $HALLD_VERSIONS/version.xml
 
 cd output/test/
 root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C++'", '1')'
