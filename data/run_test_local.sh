0091f33a16ee27202beb3c8c8b413081696a604a
Bo Yu <by51@duke.edu>
2025-08-06 14:41:38 -0400

    Test new recon config file by Igal with the addition of deuteron mass hypothesis

--- data/run_test_local.sh
+++ data/run_test_local.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm
-REACTION=phi_d_2H
+INPUTFILE=/cache/halld/RunPeriod-2021-11/rawdata/Run090213/hd_rawdata_090213_000.evio
+REACTION=recon_2021-11
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /work/halld2/home/boyu/src_software_builds/halld_versions_srcct/analysis-2021_11-ver10.xml
+gxenv /group/halld/www/halldweb/html/halld_versions/version_6.1.3.xml
 
 mkdir -p output/${REACTION}_test
 cd output/${REACTION}_test
-hd_root --config=../../configs/jana_analysis_${REACTION}.cfg ${INPUTFILE}
+hd_root --loadconfigs /work/halld2/home/boyu/src_analysis/data/configs/jana_${REACTION}.cfg ${INPUTFILE}
