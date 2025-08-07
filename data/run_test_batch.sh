e18167a826a8e973d8e3b0046af69ab7b018b938
Bo Yu <by51@duke.edu>
2025-08-07 09:02:37 -0400

    Test new recon with deuteron mass hypo added

--- data/run_test_batch.sh
+++ data/run_test_batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-REACTION=recon_2021-11
+REACTION=phi_d_2H
 RUNMIN=90211
 RUNMAX=90212
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-python launch.py configs/jobs_${REACTION}.cfg ${RUNMIN} ${RUNMAX}
+python launch.py configs/jobs_analysis_${REACTION}.cfg ${RUNMIN} ${RUNMAX}
 swif2 run -workflow src_analysis_data
