9a99c621e19cb03551524f5328630012be1c805f
Bo Yu <by51@duke.edu>
2025-08-21 17:47:52 -0400

    Test on the L1 BCAL/FCAL energy branches for Oscar

--- data/run_test_local.sh
+++ data/run_test_local.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/rawdata/Run090213/hd_rawdata_090213_000.evio
-REACTION=recon_2021-11
+INPUTFILE=/cache/halld/RunPeriod-2021-11/recon/ver04/REST/090213/dana_rest_090213_000.hddm
+REACTION=phi_d_2H
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /group/halld/www/halldweb/html/halld_versions/version_6.1.3.xml
+gxenv /group/halld/www/halldweb/html/halld_versions/version_6.3.0.xml
 
 mkdir -p output/${REACTION}_test
 cd output/${REACTION}_test
-hd_root --loadconfigs /work/halld2/home/boyu/src_analysis/data/configs/jana_${REACTION}.cfg ${INPUTFILE}
+hd_root --loadconfigs /work/halld2/home/boyu/src_analysis/data/configs/jana_analysis_${REACTION}.cfg ${INPUTFILE}
