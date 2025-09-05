7a9eeb419efbfd9576f0cbab2c3ee5138012a4a4
Bo Yu <by51@duke.edu>
2025-09-04 21:09:50 -0400

    Test phi d channel with ver04 recon launch

--- selection/run_test.sh
+++ selection/run_test.sh
@@ -2,14 +2,13 @@
 
 start=`date +%s`
 
-INPUTFILE=/work/halld2/home/oscarlin/analysis/rho_deuteron/simulation/output/model_piecewise/root/trees/tree_gd_pi0pimprotmissprot__B4_F4_T2_S2_gen_gcf_rhoMinus_model_noBKG_090221_000.root
-# INPUTFILE=/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver01/root/tree_gd_kpkmd__B4_F4_gen_coherent/*90213_000.root
-TREENAME=gd_pi0pimprotmissprot__B4_F4_T2_S2_Tree
-SELECTOR=rhoMinus_p
+INPUTFILE=/work/halld2/home/boyu/src_analysis/data/output/phi_d_2H_test/tree_gd_kpkmd__B4_F4/*.root
+TREENAME=gd_kpkmd__B4_F4_Tree
+SELECTOR=phi_d_recon
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv $HALLD_VERSIONS/version_5.21.0.xml
-# gxenv $HALLD_VERSIONS/version.xml
+# gxenv $HALLD_VERSIONS/version_5.21.0.xml
+gxenv $HALLD_VERSIONS/version.xml
 
 cd output/test/
 root -b -q $ROOT_ANALYSIS_HOME/scripts/Load_DSelector.C ../../process_chain.C'("'$INPUTFILE'", "'$TREENAME'", "'../../configs/DSelector_${SELECTOR}.C++'", '1')'
