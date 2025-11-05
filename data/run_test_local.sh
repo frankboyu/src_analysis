231a7a4f63f146b11c5d0bde8d3483a5105cf485
Bo Yu <by51@duke.edu>
2025-11-05 13:56:31 -0500

    Test new bggen sample with nucleons from deuteron

--- data/run_test_local.sh
+++ data/run_test_local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-INPUTFILE=/cache/halld/RunPeriod-2021-11/recon/ver04/REST/090213/dana_rest_090213_000.hddm
+INPUTFILE=/cache/halld/home/gxproj2/simulations/rp-2021-11-h2-27082025-rdm-bkg-flux-based-test-bggen-v2/bggen_upd-to-all_mesons/deuteron/neutron/rnb-90213/hddm/bggen_upd_090213_000_geant4_smeared.hddm
 REACTION=phi_d_2H
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
-gxenv /group/halld/www/halldweb/html/halld_versions/version_6.3.0.xml
+gxenv /group/halld/www/halldweb/html/halld_versions/version_6.5.0.xml
 
 mkdir -p output/${REACTION}_test
 cd output/${REACTION}_test
