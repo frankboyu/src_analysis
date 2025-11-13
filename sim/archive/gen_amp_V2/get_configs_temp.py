270391469dd06994d4608255ef93b0b4e00b547e
Bo Yu <by51@duke.edu>
2025-11-13 09:00:28 -0500

    Archive the code for gen_amp_v2 generator for now

---
+++ sim/archive/gen_amp_V2/get_configs_temp.py
@@ -0,0 +1,81 @@
+import sys
+import numpy as np
+
+reaction = sys.argv[1]
+
+if (reaction[-2:] == '2H'):
+    run_list = np.loadtxt("../flux/output/2H/flux_total_2H.txt")[:,0]
+elif (reaction[-3:] == '4He'):
+    run_list = np.loadtxt("../flux/output/4He/flux_total_4He.txt")[:,0]
+elif (reaction[-3:] == '12C'):
+    run_list = np.loadtxt("../flux/output/12C/flux_total_12C.txt")[:,0]
+
+pol_0_list = np.loadtxt("../plot/vm_d/polarization/outFiles/runList0_2021-11.txt")
+pol_45_list = np.loadtxt("../plot/vm_d/polarization/outFiles/runList45_2021-11.txt")
+pol_90_list = np.loadtxt("../plot/vm_d/polarization/outFiles/runList90_2021-11.txt")
+pol_135_list = np.loadtxt("../plot/vm_d/polarization/outFiles/runList135_2021-11.txt")
+
+config_file = open("configs/wrapper_"+reaction+".cfg", "r")
+config_lines = config_file.readlines()
+for (i, line) in enumerate(config_lines):
+    if (line[0:11] == 'FLUX_TO_GEN'):
+        line_flux = i
+        for (j, character) in enumerate(line):
+            if (line[j] == '9'):
+                char_flux = j
+                break
+
+    if (line[0:10] == 'POL_TO_GEN'):
+        line_pol = i
+
+    if (line[0:3] == 'BKG'):
+        line_bkg = i
+        for (j, character) in enumerate(line):
+            if (line[j] == 'R'):
+                char_bkg = j
+                break
+
+
+for run_number in run_list:
+
+    run_number = int(run_number)
+    new_config_lines = config_lines.copy()
+
+    # Replace the flux histogram for every run
+    line = config_lines[line_flux]
+    new_line = line[:char_flux] + str(run_number) + line[char_flux+5:char_flux+22] + str(run_number) + line[char_flux+27:]
+    new_config_lines[line_flux] = new_line
+
+    # Replace the polarization histogram for every run
+    if (run_number in pol_0_list):
+        line = config_lines[line_pol]
+        new_line = line[:-2] + "0\n"
+        new_config_lines[line_pol] = new_line
+    elif (run_number in pol_45_list):
+        line = config_lines[line_pol]
+        new_line = line[:-2] + "45\n"
+        new_config_lines[line_pol] = new_line
+    elif (run_number in pol_90_list):
+        line = config_lines[line_pol]
+        new_line = line[:-2] + "90\n"
+        new_config_lines[line_pol] = new_line
+    elif (run_number in pol_135_list):
+        line = config_lines[line_pol]
+        new_line = line[:-2] + "135\n"
+        new_config_lines[line_pol] = new_line
+    else:
+        new_line = "POL_TO_GEN = 0\n"
+        new_config_lines[line_pol] = new_line
+
+    # Replace the random background for runs without corresponding files
+    if (run_number == 90140 or run_number == 90615 or run_number == 90616):
+        line = config_lines[line_bkg]
+        new_line = line[:char_bkg] + "None\n"
+        new_config_lines[line_bkg] = new_line
+    else:
+        line = config_lines[line_bkg]
+        new_line = line[:char_bkg] + "Random:recon-2021_11-ver01\n"
+        new_config_lines[line_bkg] = new_line
+
+    file = open("configs/wrapper_"+reaction+"_"+str(run_number)+".cfg", "w")
+    file.writelines(new_config_lines)
