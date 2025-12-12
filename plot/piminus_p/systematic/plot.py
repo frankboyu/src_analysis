d97d903d01724df770288df297833275dd860b8a
Bo Yu <by51@duke.edu>
2025-12-12 15:56:07 -0500

    Update the scripts to get systematic errors of flux calculation

--- plot/piminus_p/systematic/plot.py
+++ plot/piminus_p/systematic/plot.py
@@ -1,14 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-bin_edges = np.loadtxt('input/flux_corr_90208.txt')
-tagh_flux_error = np.loadtxt('input/tagh_flux_syst.txt')
-tagm_flux_error = np.loadtxt('input/tagm_flux_syst.txt')
+energy_low = 6
+energy_hgh = 8
 
-plt.plot(tagh_flux_error[:,1], tagh_flux_error[:,2])
+# bin_edges = np.loadtxt('flux_corr_90208.txt')
+bin_edges = np.loadtxt('lumi_summed_2H.txt')
+tagh_flux_error = np.loadtxt('tagh_flux_syst.txt')
+tagm_flux_error = np.loadtxt('tagm_flux_syst.txt')
+
+for i in range(len(bin_edges[:,0])):
+    if bin_edges[i,0] <= 125 or bin_edges[i,0] >= 228:
+        bin_edges[i,4] = tagh_flux_error[int(bin_edges[i,1])-1,2]
+    elif bin_edges[i,0] >= 126 and bin_edges[i,0] <= 227:
+        bin_edges[i,4] = tagm_flux_error[int(bin_edges[i,1])-1,2]
+
+
+
+# plt.plot(bin_edges[:,3], bin_edges[:,6]/bin_edges[:,5]*100, marker='o', linestyle='none')
+plt.plot(bin_edges[0:126,3], bin_edges[0:126,4], color='orange')
+plt.plot(bin_edges[126:228,3], bin_edges[126:228,4], color='purple')
+plt.plot(bin_edges[228:,3], bin_edges[228:,4], color='cyan')
+# plt.plot(bin_edges[:,3], bin_edges[:,4], color='black')
+plt.fill_between([6, 8], 0, 10, color='red', alpha=0.3, label='Energy bin 1')
+plt.fill_between([8, 9], 0, 10, color='blue', alpha=0.3, label='Energy bin 2')
+plt.fill_between([9, 11], 0, 10, color='green', alpha=0.3, label='Energy bin 3')
+# plt.legend()
 plt.xlabel("Photon energy [GeV]")
 plt.ylabel("PS normalization uncertainty [%]")
-plt.savefig("output/flux_syst.png")
+plt.ylim(0, 10)
+plt.savefig("flux_syst.png")
 
 # file_output = open("output/flux_syst.txt", "w")
 
