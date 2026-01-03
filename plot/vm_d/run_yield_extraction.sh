9c01c920f17e897c16ce973a8760f3ef640c8c14
Bo Yu <by51@duke.edu>
2026-01-03 10:47:00 -0500

    Add the absolute path to the script so that it can be properly run on the batch system

--- plot/vm_d/run_yield_extraction.sh
+++ plot/vm_d/run_yield_extraction.sh
@@ -8,4 +8,4 @@ TAG=$4
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-root -b -q -l "get_yield.C(\"$CHANNEL\", \"$REACTION\", \"$OBSERVABLE\", \"$TAG\")"
+root -b -q -l "/work/halld2/home/boyu/src_analysis/plot/vm_d/get_yield.C(\"$CHANNEL\", \"$REACTION\", \"$OBSERVABLE\", \"$TAG\")"
