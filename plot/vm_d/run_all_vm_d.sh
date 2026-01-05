5c126ce03d2c043c404ff4621c5a37e48970d7dc
Bo Yu <by51@duke.edu>
2026-01-05 14:22:51 -0500

    Test new variations on dE/dx and exclusivity cuts

--- plot/vm_d/run_all_vm_d.sh
+++ plot/vm_d/run_all_vm_d.sh
@@ -2,30 +2,32 @@
 
 start=`date +%s`
 
-RUN_MODE="batch"
+# RUN_MODE="batch"
+RUN_MODE="local"
+# RUN_MODE="echo"
 
 CHANNEL_LIST=()
 CHANNEL_LIST+=("phi_d")
 # CHANNEL_LIST+=("rho_d")
 
 REACTION_LIST=()
-REACTION_LIST+=("recon_exc_data_2H" "recon_exc_sim_2H" "thrown_exc_tagged_2H")
-# REACTION_LIST+=("recon_exc_data_2H")
+# REACTION_LIST+=("recon_exc_data_2H" "recon_exc_sim_2H" "thrown_exc_tagged_2H")
+REACTION_LIST+=("recon_exc_data_2H")
 
 OBSERVABLE_LIST=()
 # OBSERVABLE_LIST+=("dsdt" "Wcostheta" "Wphi" "WPhi" "Wpsi")
 OBSERVABLE_LIST+=("dsdt")
 
 TAG_LIST=()
-# TAG_LIST+=("nominal")
-# TAG_LIST+=("dEdx_1.0" "dEdx_1.5" "dEdx_2.5" "dEdx_3.0")
-# TAG_LIST+=("misspminus_0.010_chisquared_3.5" "misspminus_0.015_chisquared_3.5" "misspminus_0.020_chisquared_3.5" "misspminus_0.025_chisquared_3.5" "misspminus_0.030_chisquared_3.5")
-# TAG_LIST+=("misspminus_0.010_chisquared_4.0" "misspminus_0.015_chisquared_4.0" "misspminus_0.020_chisquared_4.0" "misspminus_0.025_chisquared_4.0" "misspminus_0.030_chisquared_4.0")
-# TAG_LIST+=("misspminus_0.010_chisquared_5.0" "misspminus_0.015_chisquared_5.0" "misspminus_0.020_chisquared_5.0" "misspminus_0.025_chisquared_5.0" "misspminus_0.030_chisquared_5.0")
-# TAG_LIST+=("misspminus_0.010_chisquared_6.0" "misspminus_0.015_chisquared_6.0" "misspminus_0.020_chisquared_6.0" "misspminus_0.025_chisquared_6.0" "misspminus_0.030_chisquared_6.0")
-# TAG_LIST+=("misspminus_0.010_chisquared_7.0" "misspminus_0.015_chisquared_7.0" "misspminus_0.020_chisquared_7.0" "misspminus_0.025_chisquared_7.0" "misspminus_0.030_chisquared_7.0")
+TAG_LIST+=("nominal")
+TAG_LIST+=("dEdx_1.5" "dEdx_1.75" "dEdx_2.5" "dEdx_3.0")
+TAG_LIST+=("misspminus_0.010_chisquared_3.5" "misspminus_0.015_chisquared_3.5" "misspminus_0.020_chisquared_3.5" "misspminus_0.025_chisquared_3.5" "misspminus_0.030_chisquared_3.5")
+TAG_LIST+=("misspminus_0.010_chisquared_4.0" "misspminus_0.015_chisquared_4.0" "misspminus_0.020_chisquared_4.0" "misspminus_0.025_chisquared_4.0" "misspminus_0.030_chisquared_4.0")
+TAG_LIST+=("misspminus_0.010_chisquared_5.0" "misspminus_0.015_chisquared_5.0" "misspminus_0.020_chisquared_5.0" "misspminus_0.025_chisquared_5.0" "misspminus_0.030_chisquared_5.0")
+TAG_LIST+=("misspminus_0.010_chisquared_6.0" "misspminus_0.015_chisquared_6.0" "misspminus_0.020_chisquared_6.0" "misspminus_0.025_chisquared_6.0" "misspminus_0.030_chisquared_6.0")
+TAG_LIST+=("misspminus_0.010_chisquared_7.0" "misspminus_0.015_chisquared_7.0" "misspminus_0.020_chisquared_7.0" "misspminus_0.025_chisquared_7.0" "misspminus_0.030_chisquared_7.0")
 # TAG_LIST+=("momentum_0.400" "momentum_0.425" "momentum_0.475" "momentum_0.500")
-# TAG_LIST+=("theta_1.0" "theta_1.5" "theta_2.5" "theta_3.0")
+TAG_LIST+=("theta_1.8" "theta_1.9" "theta_2.1" "theta_2.2")
 # TAG_LIST+=("vertexZ_13.0" "vertexZ_13.5" "vertexZ_14.5" "vertexZ_15.0")
 # TAG_LIST+=("vertexR_0.50" "vertexR_0.75" "vertexR_1.25" "vertexR_1.50")
 # TAG_LIST+=("fitfunc_quadratic" "fitfunc_phenomenological" "fitfunc_fulllinear" "fitfunc_fullquadratic")
@@ -35,9 +37,20 @@ TAG_LIST=()
 # TAG_LIST+=("fitwidth_0.0040" "fitwidth_0.0048" "fitwidth_0.0060" "fitwidth_0.0075")
 # TAG_LIST+=("fitbkg_fulllinear" "fitbkg_quadratic" "fitbkg_fullquadratic" "fitbkg_phenomenological")
 # TAG_LIST+=("fitsig_noBL" "fitsig_nonrel" "fitsig_relBWsim")
-# TAG_LIST+=("simweight_pass0" "simweight_pass1" "simweight_pass2" "simweight_pass3")
-TAG_LIST+=("simweight_pass6")
-
+# TAG_LIST+=("simweight_iter0" "simweight_iter1" "simweight_iter2" "simweight_iter3" "simweight_iter4" "simweight_iter5" "simweight_iter6")
+# for i in {-1..1}
+# do
+#     for j in {-1..1}
+#     do
+#         for k in {-1..1}
+#         do
+#             for l in {-1..1}
+#             do
+#                 TAG_LIST+=("simweight_syst_a1_${i}_b1_${j}_a2_${k}_b2_${l}")
+#             done
+#         done
+#     done
+# done
 
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
