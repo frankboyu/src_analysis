8ad0369defacdcf8e03ccf4a68e6d5bf2551bf8c
Bo Yu <by51@duke.edu>
2026-02-01 10:28:50 -0500

    Add new versions to cache script

--- data/run_jcache.sh
+++ data/run_jcache.sh
@@ -11,18 +11,18 @@ then
 elif [ "${REACTION}" == "phi_d_2H_ver11" ]
 then
     DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_kpkmd__B4_F4/merged")
-    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_kpkmdinc__B4_F4_T1_S2/merged")
+elif [ "${REACTION}" == "phi_d_2H_ver12" ]
+then
+    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver12/tree_gd_kpkmd__B5_F4/merged")
 elif [ "${REACTION}" == "rho_d_2H_ver10" ]
 then
     DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver10/tree_gd_pippimd__B4_F4/merged")
 elif [ "${REACTION}" == "rho_d_2H_ver11" ]
 then
     DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pippimd__B4_F4/merged")
-    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pippimdinc__B4_F4_T1_S2/merged")
 elif [ "${REACTION}" == "omega_d_2H_ver11" ]
 then
     DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pi0pippimd__B4_F4/merged")
-    DIR_LIST+=("/mss/halld/RunPeriod-2021-11/analysis/ver11/tree_gd_pi0pippimdinc__B4_F4_T1_S2/merged")
 fi
 
 for DIR in "${DIR_LIST[@]}"
