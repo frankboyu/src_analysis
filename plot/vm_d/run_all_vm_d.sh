8d421f76e3622857f6590a5985c3225b450437d0
Bo Yu <by51@duke.edu>
2026-02-10 21:29:35 -0500

    Update cut variations in the run script

--- plot/vm_d/run_all_vm_d.sh
+++ plot/vm_d/run_all_vm_d.sh
@@ -11,29 +11,30 @@ CHANNEL_LIST+=("phi_d")
 # CHANNEL_LIST+=("rho_d")
 
 REACTION_LIST=()
-# REACTION_LIST+=("recon_exc_sim_2H_ver12_flat" "thrown_exc_tagged_2H_ver12_flat")
 REACTION_LIST+=("recon_exc_data_2H_ver12")
+REACTION_LIST+=("recon_exc_sim_2H_ver12_flat" "thrown_exc_tagged_2H_ver12_flat")
+REACTION_LIST+=("recon_exc_sim_2H_ver12_model" "thrown_exc_tagged_2H_ver12_model")
 
 OBSERVABLE_LIST=()
+# OBSERVABLE_LIST+=("Wcostheta" "dsdt")
 OBSERVABLE_LIST+=("dsdt" "Wcostheta" "Wdecayphi" "Wpolphi" "Wpsi")
-# OBSERVABLE_LIST+=("dsdt")
 
 TAG_LIST=()
 TAG_LIST+=("nominal")
-# TAG_LIST+=("dEdx_1.50" "dEdx_1.75" "dEdx_2.50" "dEdx_3.00")
-TAG_LIST+=("misspminus_0.010" "misspminus_0.015" "misspminus_0.025" "misspminus_0.030")
-TAG_LIST+=("chisquared_3.5" "chisquared_4.0" "chisquared_5.0" "chisquared_6.0")
-TAG_LIST+=("momentum_0.400" "momentum_0.425" "momentum_0.475" "momentum_0.500")
-TAG_LIST+=("theta_1.8" "theta_1.9" "theta_2.1" "theta_2.2")
-# TAG_LIST+=("vertexZ_13.0" "vertexZ_13.5" "vertexZ_14.5" "vertexZ_15.0")
-# TAG_LIST+=("vertexR_0.50" "vertexR_0.75" "vertexR_1.25" "vertexR_1.50")
-# TAG_LIST+=("fitfunc_quadratic" "fitfunc_phenomenological" "fitfunc_fulllinear" "fitfunc_fullquadratic")
-# TAG_LIST+=("beamaccid_3" "beamaccid_5" "beamaccid_4out")
-# TAG_LIST+=("comboaccid_all" "comboaccid_none")
-# TAG_LIST+=("fitmax_1.06" "fitmax_1.07" "fitmax_1.09" "fitmax_1.10")
-# TAG_LIST+=("fitwidth_0.0040" "fitwidth_0.0048" "fitwidth_0.0060" "fitwidth_0.0075")
-# TAG_LIST+=("fitbkg_fulllinear" "fitbkg_quadratic" "fitbkg_fullquadratic" "fitbkg_phenomenological")
-# TAG_LIST+=("fitsig_noBL" "fitsig_nonrel" "fitsig_relBWsim")
+TAG_LIST+=("dEdx_1.50" "dEdx_1.75" "dEdx_2.50" "dEdx_3.00")
+TAG_LIST+=("misspminus_0.0150" "misspminus_0.0175" "misspminus_0.0250" "misspminus_0.0300")
+TAG_LIST+=("chisquared_4.50" "chisquared_4.75" "chisquared_5.50" "chisquared_6.00")
+TAG_LIST+=("momentum_0.350" "momentum_0.375" "momentum_0.425" "momentum_0.450")
+TAG_LIST+=("theta_1.90" "theta_1.95" "theta_2.05" "theta_2.10")
+TAG_LIST+=("vertexZ_13.50" "vertexZ_13.75" "vertexZ_14.25" "vertexZ_14.50")
+TAG_LIST+=("vertexR_0.50" "vertexR_0.75" "vertexR_1.25" "vertexR_1.50")
+TAG_LIST+=("fitfunc_quadratic" "fitfunc_phenomenological" "fitfunc_fulllinear" "fitfunc_fullquadratic")
+TAG_LIST+=("beamaccid_3" "beamaccid_5" "beamaccid_4out")
+TAG_LIST+=("comboaccid_all" "comboaccid_none")
+TAG_LIST+=("fitmax_1.06" "fitmax_1.07" "fitmax_1.09" "fitmax_1.10")
+TAG_LIST+=("fitwidth_0.0040" "fitwidth_0.0048" "fitwidth_0.0060" "fitwidth_0.0075")
+TAG_LIST+=("fitbkg_fulllinear" "fitbkg_quadratic" "fitbkg_fullquadratic" "fitbkg_phenomenological")
+TAG_LIST+=("fitsig_noBL" "fitsig_nonrel" "fitsig_relBWsim")
 # TAG_LIST+=("simweight_iter0" "simweight_iter1" "simweight_iter2" "simweight_iter3" "simweight_iter4" "simweight_iter5" "simweight_iter6")
 # for i in {-1..1}
 # do
@@ -70,6 +71,9 @@ do
                     continue
                 fi
                 if [[ "$REACTION" == *"data"* && "$TAG" == *"simweight"* ]]; then
+                    continue
+                fi
+                if [[ "$REACTION" == *"data"* && "$TAG" == "sideband" ]]; then
                     continue
                 fi
                 if [[ "$OBSERVABLE" == "dsdt" && "$TAG" == "sideband" ]]; then
