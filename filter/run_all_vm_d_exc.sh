e38df459e0c8794c503f142eeda1624aefe75b3f
Bo Yu <by51@duke.edu>
2026-03-03 11:40:29 -0500

    Make new filtered histograms for all phi_d files

--- filter/run_all_vm_d_exc.sh
+++ filter/run_all_vm_d_exc.sh
@@ -3,19 +3,19 @@
 start=`date +%s`
 
 # data
-sh run_local.sh 'phi_d_recon_exc' 'data_2H_ver12'           'both'
+sh run_local.sh 'phi_d_recon_exc' 'data_2H_ver12'           'hist'
 
 # sim
-sh run_local.sh 'phi_d_recon_exc' 'sim_2H_ver12_flat'       'both'
-sh run_local.sh 'phi_d_recon_exc' 'sim_2H_ver12_model'      'both'
+sh run_local.sh 'phi_d_recon_exc' 'sim_2H_ver12_flat'       'hist'
+sh run_local.sh 'phi_d_recon_exc' 'sim_2H_ver12_model'      'hist'
 
 # tagged
-sh run_local.sh 'phi_d_thrown_exc' 'tagged_2H_ver12_flat'   'both'
-sh run_local.sh 'phi_d_thrown_exc' 'tagged_2H_ver12_model'  'both'
+sh run_local.sh 'phi_d_thrown_exc' 'tagged_2H_ver12_flat'   'hist'
+sh run_local.sh 'phi_d_thrown_exc' 'tagged_2H_ver12_model'  'hist'
 
 # gen
-sh run_local.sh 'phi_d_thrown_exc' 'gen_2H_ver12_flat'      'both'
-sh run_local.sh 'phi_d_thrown_exc' 'gen_2H_ver12_model'     'both'
+sh run_local.sh 'phi_d_thrown_exc' 'gen_2H_ver12_flat'      'hist'
+sh run_local.sh 'phi_d_thrown_exc' 'gen_2H_ver12_model'     'hist'
 
 # hists
 # python configs/hist_vm_d.py
