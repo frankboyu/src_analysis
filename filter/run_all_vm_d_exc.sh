4a4046c288b1be10ec388315cd38d7092b67d6d5
Bo Yu <by51@duke.edu>
2026-05-30 09:58:38 -0400

    Edit phi d filter run script to reflect the changes in naming conventions

--- filter/run_all_vm_d_exc.sh
+++ filter/run_all_vm_d_exc.sh
@@ -3,19 +3,19 @@
 start=`date +%s`
 
 # data
-sh run_local.sh 'phi_d_exc_recon'   'data_2H_ver12'     'both'
+sh run_local.sh 'phi_d_exc_recon'   'data_ver12'     'both'
 
 # sim
-sh run_local.sh 'phi_d_exc_recon'   'sim_2H_ver12'      'both'
+sh run_local.sh 'phi_d_exc_recon'   'sim_ver12'      'both'
 
 # tagged
-sh run_local.sh 'phi_d_exc_thrown'  'tagged_2H_ver12'   'both'
+sh run_local.sh 'phi_d_exc_thrown'  'tagged_ver12'   'both'
 
 # gen
-sh run_local.sh 'phi_d_exc_thrown'  'gen_2H_ver12'      'both'
+sh run_local.sh 'phi_d_exc_thrown'  'gen_ver12'      'both'
 
 # hists
-# python configs/hist_vm_d.py
+# python configs/hist_phi_d_exc.py
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
