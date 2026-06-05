1ea96c34e1f4ed5459eeec8e2bdd07605705e8cb
Bo Yu <by51@duke.edu>
2026-06-05 08:40:25 -0400

    Edit the phi d DSelector run script to the newest simulation

--- selection/run_all_vm_d_exc.sh
+++ selection/run_all_vm_d_exc.sh
@@ -7,15 +7,15 @@ sh run_local.sh     'phi_d_exc_recon'   '/work/halld2/home/boyu/src_analysis/dat
 sh run_rename.sh    'phi_d_exc_recon'   'data' 'ver12'
 
 # sim
-sh run_local.sh     'phi_d_exc_recon'   '/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_03/root/tree_gd_kpkmd__B5_F4_gen_coherent/*.root'     'gd_kpkmd__B5_F4_Tree'
+sh run_local.sh     'phi_d_exc_recon'   '/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_05/root/tree_gd_kpkmd__B5_F4_gen_coherent/*.root'     'gd_kpkmd__B5_F4_Tree'
 sh run_rename.sh    'phi_d_exc_recon'   'sim' 'ver12'
 
 # gen
-hadd output/selectedtree_phi_d_exc_thrown.root  /work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_03/root/generator/*.root
+hadd output/selectedtree_phi_d_exc_thrown.root  /work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_05/root/generator/*.root
 sh run_rename.sh    'phi_d_exc_thrown'  '' 'gen_ver12'
 
 # tagged
-sh run_local.sh     'phi_d_exc_thrown'  '/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_03/root/thrown/*.root'  'Thrown_Tree'
+sh run_local.sh     'phi_d_exc_thrown'  '/work/halld2/home/boyu/src_analysis/sim/output/phi_d_2H_ver12_05/root/thrown/*.root'  'Thrown_Tree'
 sh run_rename.sh    'phi_d_exc_thrown'  '' 'tagged_ver12'
 
 end=`date +%s`
