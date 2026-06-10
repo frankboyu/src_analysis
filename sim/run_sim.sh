7eb2e7d2aaf4e9e91633abe56603f80b3dc1810e
Bo Yu <by51@duke.edu>
2026-06-10 15:20:15 -0400

    Decrease the number of events per file in the simulation run script, as 1M seems too many to finish

--- sim/run_sim.sh
+++ sim/run_sim.sh
@@ -15,7 +15,7 @@ python get_number.py ${REACTION} ${EVENTS}    # Generate a text file of the numb
 while read -r run number;
 do
     echo ${run} ${number}
-    gluex_MC.py configs/wrapper_${REACTION}_${run}.cfg ${run} ${number} per_file=1000000 batch=1 logdir=/farm_out/boyu/src_analysis/sim/${REACTION}
+    gluex_MC.py configs/wrapper_${REACTION}_${run}.cfg ${run} ${number} per_file=100000 batch=1 logdir=/farm_out/boyu/src_analysis/sim/${REACTION}
 done < list.txt
 
 swif2 run -workflow src_analysis_sim_2H
