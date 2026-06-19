e32d107fb2d7c107e0d577172b228e70c9e4b0f7
Bo Yu <by51@duke.edu>
2026-06-19 13:15:35 -0400

    Run reconstruction and skim on empty target data and archive the config files

--- data/run_skim.sh
+++ data/run_skim.sh
@@ -5,5 +5,5 @@ REACTION=$1
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 
-python launch.py configs/jobs_analysis_${REACTION}.cfg 90001 90662
+python launch.py configs/jobs_${REACTION}.cfg 90001 90662
 swif2 run -workflow src_analysis_data -maxconcurrent 1000
