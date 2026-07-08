bea6605f9131a14c5400beab68f862d834f6864d
Bo Yu <by51@duke.edu>
2026-07-08 09:21:54 -0400

    Add duplicate scripts to allow parallel running theory code for different energies

--- plot/vm_d/theory/run_mass_produce_16.sh
+++ plot/vm_d/theory/run_mass_produce_16.sh
@@ -5,9 +5,9 @@ start=`date +%s`
 LABEL=four_para
 mkdir output/${LABEL}/
 
-gfortran -ffixed-line-length-none -o run.out edved_wkng_pol.f
+gfortran -ffixed-line-length-none -o run_16.out edved_wkng_pol.f
 
-for BEAMENERGY in 2.1 3.1 6.9 8.3 9.7
+for BEAMENERGY in 2.1
 do
     mkdir output/${LABEL}/E_${BEAMENERGY}/
     for SGAMMAN in $(seq 10 0.2 12)
@@ -20,12 +20,12 @@ do
                 for BPHIN in $(seq 5 1.0 15)
                 do
                     echo "Running with E=$BEAMENERGY GeV, sigma_gn=$SGAMMAN mb, b_gn=$BGAMMAN GeV^-2, sigma_vn=$SPHIN mb, b_vn=$BPHIN GeV^-2"
-                    echo "$BEAMENERGY" > input_paras.txt
-                    echo "$SGAMMAN" >> input_paras.txt
-                    echo "$BGAMMAN" >> input_paras.txt
-                    echo "$SPHIN" >> input_paras.txt
-                    echo "$BPHIN" >> input_paras.txt
-                    ./run.out > output/${LABEL}/E_${BEAMENERGY}/E_${BEAMENERGY}_gamma_s_${SGAMMAN}_b_${BGAMMAN}/E_${BEAMENERGY}_gamma_s_${SGAMMAN}_b_${BGAMMAN}_phi_s_${SPHIN}_b_${BPHIN}.txt
+                    echo "$BEAMENERGY" > input_paras_16.txt
+                    echo "$SGAMMAN" >> input_paras_16.txt
+                    echo "$BGAMMAN" >> input_paras_16.txt
+                    echo "$SPHIN" >> input_paras_16.txt
+                    echo "$BPHIN" >> input_paras_16.txt
+                    ./run_16.out > output/${LABEL}/E_${BEAMENERGY}/E_${BEAMENERGY}_gamma_s_${SGAMMAN}_b_${BGAMMAN}/E_${BEAMENERGY}_gamma_s_${SGAMMAN}_b_${BGAMMAN}_phi_s_${SPHIN}_b_${BPHIN}.txt
                 done
             done
         done
