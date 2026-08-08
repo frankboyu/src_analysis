b42b62897d91dbb606bcdf1fdc86e714abd0a399
Bo Yu <by51@duke.edu>
2026-08-08 09:34:37 -0400

    Update the phi d filter run script

--- filter/run_all_phi_exc.sh
+++ filter/run_all_phi_exc.sh
@@ -4,16 +4,15 @@ start=`date +%s`
 
 # data
 sh run_local.sh 'phi_d_exc_recon'   'data_ver12'     'both'
-# sh run_local.sh 'phi_d_exc_recon'   'data_ver12_empty'     'both'
 
 # sim
-sh run_local.sh 'phi_d_exc_recon'   'sim_ver12'      'hist'
+sh run_local.sh 'phi_d_exc_recon'   'sim_ver12_07'      'both'
 
 # tagged
-sh run_local.sh 'phi_d_exc_thrown'  'tagged_ver12'   'both'
+sh run_local.sh 'phi_d_exc_thrown'  'tagged_ver12_07'   'both'
 
 # gen
-sh run_local.sh 'phi_d_exc_thrown'  'gen_ver12'      'both'
+sh run_local.sh 'phi_d_exc_thrown'  'gen_ver12_07'      'both'
 
 end=`date +%s`
 echo "Time taken: $(echo "scale=2; ($end - $start) / 60" | bc -l) minutes"
