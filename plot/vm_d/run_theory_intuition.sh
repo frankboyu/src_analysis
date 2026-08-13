a455bad117ea1df6d0fed048f3a2b7a87c2eb40a
Bo Yu <by51@duke.edu>
2026-08-13 13:17:48 -0400

    Test new best fitting results for theory intuition

--- plot/vm_d/run_theory_intuition.sh
+++ plot/vm_d/run_theory_intuition.sh
@@ -1,8 +1,8 @@
 echo 6.9   > input/theory_paras.txt
 echo 11.0  >> input/theory_paras.txt
 echo 4.00  >> input/theory_paras.txt
-echo 40.0  >> input/theory_paras.txt
-echo 10.00 >> input/theory_paras.txt
+echo 30.8  >> input/theory_paras.txt
+echo 11.2 >> input/theory_paras.txt
 
 gfortran -ffixed-line-length-none -o run_theory.out get_edved_wkng_pol.f
 ./run_theory.out > output/table_phi_d_theory_intuition.txt
